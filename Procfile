6ae083dcdda82dc850b63f5f4451d48cba6383a3
Thibault Jouannic <thibault@miximum.fr>
2021-07-07 10:51:59 +0200

    Create the starting script for Scalingo

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn config.wsgi:application
+web: bash bin/start.sh
---
+++ bin/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script is ran by scalingo to start the application
+
+echo "Deploying the EnvErgo Django app ($DJANGO_SETTINGS_MODULE)"
+python manage.py compilemessages
+python manage.py collectstatic --noinput
+python manage.py compress --force
+gunicorn config.wsgi:application --log-file -
+echo "Completed start web script"
