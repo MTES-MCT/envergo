9ba41b6ad6bedd48a181cdbaba0b5ba72521a60a
Thibault Jouannic <thibault@miximum.fr>
2021-07-08 14:51:12 +0200

    Speedup scalingo deployment

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,7 @@
 # This script is ran by scalingo to start the application
 
 echo "Deploying the EnvErgo Django app ($DJANGO_SETTINGS_MODULE)"
-python manage.py compilemessages
+python manage.py compilemessages -l fr -i .scalingo
 python manage.py collectstatic --noinput
 python manage.py compress --force
 gunicorn config.wsgi:application --log-file -
