3ed99d733119e2d0a48ef8fe17806d9aaa5bcc17
Thibault Jouannic <thibault@miximum.fr>
2021-08-16 14:21:11 +0200

    Add new fields to evaluation admin module

--- envergo/evaluations/admin.py
+++ envergo/evaluations/admin.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from envergo.evaluations.forms import EvaluationFormMixin
 from envergo.evaluations.models import Evaluation
@@ -13,3 +14,31 @@ class EvaluationAdminForm(EvaluationFormMixin, forms.ModelForm):
 class EvaluationAdmin(admin.ModelAdmin):
     list_display = ["application_number", "created_at"]
     form = EvaluationAdminForm
+
+    fieldsets = (
+        (None, {"fields": ("application_number",)}),
+        (
+            _("Project data"),
+            {
+                "fields": (
+                    "commune",
+                    "created_surface",
+                    "existing_surface",
+                )
+            },
+        ),
+        (
+            _("Evaluation report"),
+            {
+                "fields": (
+                    "global_probability",
+                    "rainwater_runoff_probability",
+                    "rainwater_runoff_impact",
+                    "flood_zone_probability",
+                    "flood_zone_impact",
+                    "wetland_probability",
+                    "wetland_impact",
+                )
+            },
+        ),
+    )
