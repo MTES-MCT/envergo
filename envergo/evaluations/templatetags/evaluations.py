71ae1282d92d13ed8a214015e1a166ad54ab5c01
Thibault Jouannic <thibault@miximum.fr>
2021-08-19 16:52:32 +0200

    Correctly display the gauge and needle

--- envergo/evaluations/templatetags/evaluations.py
+++ envergo/evaluations/templatetags/evaluations.py
@@ -19,3 +19,11 @@ def probability(p):
 
     display = f'<span class="probability probability-{p}">{label}</span>'
     return mark_safe(display)
+
+
+@register.simple_tag
+def gauge_angle(p):
+    """Fetches the correct svg gauge needle angle."""
+
+    angles = [-79, -47, -16, 16, 48, 79]
+    return angles[p]
