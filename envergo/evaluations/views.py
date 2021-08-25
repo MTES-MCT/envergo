1a7926b6da146ca9e7ef479a4511635c38aa37c2
Thibault Jouannic <thibault@miximum.fr>
2021-08-25 11:22:54 +0200

    Fix a broken tests on evaluation not found page

--- envergo/evaluations/views.py
+++ envergo/evaluations/views.py
@@ -49,5 +49,7 @@ class EvaluationDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["criterions"] = self.object.criterions.all()
+
+        if self.object:
+            context["criterions"] = self.object.criterions.all()
         return context
