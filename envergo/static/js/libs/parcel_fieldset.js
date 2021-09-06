252a4b9a0b901aaa8fbf49c1d3ca7d5445127863
Thibault Jouannic <thibault@miximum.fr>
2021-09-06 10:26:51 +0200

    Use javascript to dynamically add new parcel forms

--- envergo/static/js/libs/parcel_fieldset.js
+++ envergo/static/js/libs/parcel_fieldset.js
@@ -1,7 +1,10 @@
+/**
+ * Handles the dynamic parcel formset feature.
+ */
 (function (exports) {
     'use strict';
 
-    const Fieldset = function (formPrefix) {
+    const Formset = function (formPrefix) {
         this.formPrefix = formPrefix;
         this.inputs = {
             totalFormsInput: document.getElementById(`id_${formPrefix}-TOTAL_FORMS`),
@@ -9,13 +12,48 @@
             minNumFormsInput: document.getElementById(`id_${formPrefix}-MIN_NUM_FORMS`),
             maxNumFormsInput: document.getElementById(`id_${formPrefix}-MAX_NUM_FORMS`),
         };
-        this.addBtn = document.getElementById('btn-add-parcels');
+        this.formTemplate = document.getElementById(`tpl-form-${formPrefix}`);
+        this.formset = document.getElementById(`formset-${formPrefix}`);
+        this.addBtn = document.getElementById('btn-add-parcel');
+        this.addBtn.addEventListener('click', this.onAddFormButtonClicked.bind(this));
+
+        this.state = {
+            totalForms: parseInt(this.inputs.totalFormsInput.value)
+        };
     };
-    exports.Fieldset = Fieldset;
+    exports.Formset = Formset;
+
+    /**
+     * Update the application state by increasing the number of required forms.
+     */
+    Formset.prototype.onAddFormButtonClicked = function (e) {
+        this.state.totalForms += 1;
+        this.render();
+    };
+
+    /**
+     * Update the dom depending on current state
+     */
+    Formset.prototype.render = function () {
+        const nbCurrentForms = parseInt(this.inputs.totalFormsInput.value);
+        const nbTargetForms = this.state.totalForms;
+        const delta = nbTargetForms - nbCurrentForms;
+
+        // Use the empty form template to add new forms to the formset
+        for (let i = 0; i < delta; i++) {
+            let formId = nbCurrentForms + i;
+            const newForm = this.formTemplate.cloneNode(true);
+            newForm.innerHTML = newForm.innerHTML.replaceAll('__prefix__', formId);
+            this.formset.appendChild(newForm.content);
+
+        }
+
+        // Update the TOTAl_FORMS input value
+        this.inputs.totalFormsInput.value = nbTargetForms;
+    };
 })(this);
 
 
-
 window.onload = function () {
-    new Fieldset('parcel');
+    new Formset('parcel');
 };
