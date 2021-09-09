96c357a7cc4bdece06a10b8cae6f6eadce11ea71
Thibault Jouannic <thibault@miximum.fr>
2021-09-09 16:53:23 +0200

    Fix module path error

--- config/celery_app.py
+++ config/celery_app.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 from django.conf import settings  # noqa
 
