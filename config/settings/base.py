a47a708c34fe7d1dffc3c902e06925b4c6913ff5
Thibault Jouannic <thibault@miximum.fr>
2021-10-08 15:40:09 +0200

    Disable useless allauth auth backend

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,7 +87,6 @@ MIGRATION_MODULES = {"sites": "envergo.contrib.sites.migrations"}
 # https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
