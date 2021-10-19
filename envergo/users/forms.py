51d404e358c0ba976e062327859007ea5c4ed351
Thibault Jouannic <thibault@miximum.fr>
2021-10-19 11:09:03 +0200

    Fix login form error message bad translation

--- envergo/users/forms.py
+++ envergo/users/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
 
 from envergo.users.models import User
+
+# This string is used in django's original AuthenticationForm
+# There is a typo in the string translation, so we add this variable here
+# so it is caught by `makemessages` and we can override it in our own
+# locale file.
+_INVALID_LOGIN_ERROR_MSG = (
+    _(
+        "Please enter a correct %(username)s and password. Note that both "
+        "fields may be case-sensitive."
+    ),
+)
 
 
 class RegisterForm(UserCreationForm):
