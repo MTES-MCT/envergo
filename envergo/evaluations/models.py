f92aa758d1261f73c6bbea4d00dbdb2e55add500
Thibault Jouannic <thibault@miximum.fr>
2021-10-26 11:25:21 +0200

    Remove the commune field entirely

--- envergo/evaluations/models.py
+++ envergo/evaluations/models.py
@@ -77,12 +77,6 @@ class Evaluation(models.Model):
         validators=[FileExtensionValidator(allowed_extensions=["pdf"])],
     )
 
-    commune = models.CharField(
-        _("Commune"),
-        max_length=256,
-        blank=True,
-        help_text=_("The name and postcode of the project commune"),
-    )
     address = models.TextField(_("Address"))
     created_surface = models.IntegerField(
         _("Created surface"), help_text=_("In square meters")
