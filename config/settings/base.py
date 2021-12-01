3c519210c75763fe9993b35c125431014fb5ed81
Thibault Jouannic <thibault@miximum.fr>
2021-12-01 10:37:43 +0100

    Configure a custom file storage for user uploads

--- config/settings/base.py
+++ config/settings/base.py
@@ -68,6 +68,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     "phonenumber_field",
     "leaflet",
+    "formtools",
 ]
 
 LOCAL_APPS = [
@@ -257,6 +258,9 @@ LOGGING = {
 # https://django-compressor.readthedocs.io/en/latest/quickstart/#installation
 INSTALLED_APPS += ["compressor"]
 STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
+
+# Handle file uploads
+UPLOAD_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 # CELERY
 if USE_TZ:
--- envergo/utils/storages.py
+++ envergo/utils/storages.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -9,3 +10,8 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+
+
+class UploadS3Boto3Storage(S3Boto3Storage):
+    location = "upload"
+    bucket_name = settings.AWS_UPLOAD_BUCKET_NAME
