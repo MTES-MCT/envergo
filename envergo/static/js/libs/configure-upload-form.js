832cfe26008b102f366d5219e7d4108df7aeb361
Thibault Jouannic <thibault@miximum.fr>
2021-12-03 15:38:23 +0100

    Use dropzone to preview and submit files

--- envergo/static/js/libs/configure-upload-form.js
+++ envergo/static/js/libs/configure-upload-form.js
@@ -1,13 +1,28 @@
 window.addEventListener('load', function() {
-  let dropzone = new Dropzone('form#upload-form', {
-    url: '/',
-    paramName: 'files',
+
+  var form = document.getElementById(DROPZONE_FORM);
+  var field = document.getElementById(DROPZONE_FIELD);
+  var previewElt = document.getElementById('dropzone-previews');
+  previewElt.classList.add('dropzone');
+
+  var dropzone = new Dropzone(form, {
+    url: form.action,
+    paramName: 'additional_files',
     maxFilesize: 20,
     maxFiles: 10,
     acceptedFiles: 'image/*,application/pdf,application/zip',
     autoProcessQueue: false,
     uploadMultiple: true,
     parallelUploads: 100,
-    maxFiles: 100
+    maxFiles: 100,
+    addRemoveLinks: true,
+    previewsContainer: '.dropzone-previews',
+    init: function() {
+      form.addEventListener('submit', function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        this.processQueue();
+      }.bind(this));
+    }
   });
 });
