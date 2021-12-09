2238f168cf1883626b3e3829466dc2f90d3c90e1
Thibault Jouannic <thibault@miximum.fr>
2021-12-09 15:51:11 +0100

    Refactor and translate urls

--- envergo/evaluations/urls.py
+++ envergo/evaluations/urls.py
@@ -17,41 +17,34 @@ from envergo.evaluations.views import (
 urlpatterns = [
     path("", EvaluationSearch.as_view(), name="evaluation_search"),
     path(
-        _("requests/"),
+        _("form/"),
         include(
             [
                 path("", RequestEvalWizardReset.as_view(), name="request_evaluation"),
                 path(
-                    _("form/"),
-                    include(
-                        [
-                            path(
-                                "",
-                                RequestEvalWizardReset.as_view(),
-                                name="request_eval_wizard_reset",
-                            ),
-                            path(
-                                _("step-1/"),
-                                RequestEvalWizardStep1.as_view(),
-                                name="request_eval_wizard_step_1",
-                            ),
-                            path(
-                                _("step-2/"),
-                                RequestEvalWizardStep2.as_view(),
-                                name="request_eval_wizard_step_2",
-                            ),
-                            path(
-                                _("step-files/"),
-                                RequestEvalWizardStepFiles.as_view(),
-                                name="request_eval_wizard_step_files",
-                            ),
-                            path(
-                                _("confirm/"),
-                                RequestEvalWizardSubmit.as_view(),
-                                name="request_eval_wizard_submit",
-                            ),
-                        ]
-                    ),
+                    "",
+                    RequestEvalWizardReset.as_view(),
+                    name="request_eval_wizard_reset",
+                ),
+                path(
+                    _("step-1/"),
+                    RequestEvalWizardStep1.as_view(),
+                    name="request_eval_wizard_step_1",
+                ),
+                path(
+                    _("step-2/"),
+                    RequestEvalWizardStep2.as_view(),
+                    name="request_eval_wizard_step_2",
+                ),
+                path(
+                    _("step-files/"),
+                    RequestEvalWizardStepFiles.as_view(),
+                    name="request_eval_wizard_step_files",
+                ),
+                path(
+                    _("confirm/"),
+                    RequestEvalWizardSubmit.as_view(),
+                    name="request_eval_wizard_submit",
                 ),
                 path(_("success/"), RequestSuccess.as_view(), name="request_success"),
             ]
