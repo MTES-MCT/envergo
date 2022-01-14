025fa6d11494738cabbe75fe11aad4b473e6052b
Thibault Jouannic <thibault@miximum.fr>
2022-01-14 13:51:09 +0100

    Fix dashboard issues

--- envergo/evaluations/templatetags/evaluations.py
+++ envergo/evaluations/templatetags/evaluations.py
@@ -1,6 +1,8 @@
 from django import template
 from django.utils.safestring import mark_safe
 
+from envergo.evaluations.models import RESULTS
+
 register = template.Library()
 
 
@@ -18,6 +20,22 @@ def probability(p):
     label = p_labels[p]
 
     display = f'<span class="fr-tag probability probability-{p}">{label}</span>'
+    return mark_safe(display)
+
+
+@register.simple_tag
+def result_tag(evaluation):
+
+    result = evaluation.get_result_display()
+    proba_css = {
+        RESULTS.soumis: 4,
+        RESULTS.action_requise: 3,
+        RESULTS.non_soumis: 2,
+    }
+    proba_level = proba_css.get(evaluation.result)
+    display = (
+        f'<span class="fr-tag probability probability-{proba_level}">{result}</span>'
+    )
     return mark_safe(display)
 
 
