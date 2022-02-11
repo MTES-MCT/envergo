05fef4f6483a5aced054b6ce07ede4b132760a8a
Thibault Jouannic <thibault@miximum.fr>
2022-02-11 10:21:53 +0100

    Fix faulty moulinette.apps config

--- envergo/moulinette/apps.py
+++ envergo/moulinette/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MoulinetteConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "moulinette"
+    name = "envergo.moulinette"
