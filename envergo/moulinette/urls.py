1068a0ef2586c210217cbcf33a01aa2b1eb4d58b
Thibault Jouannic <thibault@miximum.fr>
2022-02-22 10:31:35 +0100

    Use a single view to render moulinette result

--- envergo/moulinette/urls.py
+++ envergo/moulinette/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from django.utils.translation import gettext_lazy as _
 
-from envergo.moulinette.views import MoulinetteHome, MoulinetteResult
+from envergo.moulinette.views import MoulinetteHome
 
 urlpatterns = [
     path("", MoulinetteHome.as_view(), name="moulinette_home"),
-    path(_("result/"), MoulinetteResult.as_view(), name="moulinette_result"),
 ]
--- envergo/moulinette/views.py
+++ envergo/moulinette/views.py
@@ -1,72 +1,82 @@
-import base64
-import binascii
-
-from django.core.exceptions import BadRequest
 from django.http import HttpResponseRedirect, QueryDict
 from django.urls import reverse
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView
 
 from envergo.moulinette.forms import MoulinetteForm
 from envergo.moulinette.models import Moulinette
 
 
 class MoulinetteHome(FormView):
-    template_name = "moulinette/home.html"
+    """Display the moulinette form and results.
+
+    When the form is submitted, we urlencode the form parameters and redirect
+    to the same url with the added GET parameters.
+
+    Also, the "coords" attribute is base64 encoded.
+    """
+
     form_class = MoulinetteForm
 
-    def form_valid(self, form):
+    def get_initial(self):
+        return self.request.GET
 
-        form_data = form.cleaned_data
-        coords = form_data["coords"]
-        coords_b64 = base64.urlsafe_b64encode(coords.ewkt.encode()).decode()
-        url_data = {
-            "created_surface": form_data["created_surface"],
-            "existing_surface": form_data["existing_surface"],
-            "coords": coords_b64,
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = {
+            "initial": self.get_initial(),
+            "prefix": self.get_prefix(),
         }
-        get = QueryDict("", mutable=True)
-        get.update(url_data)
-        url_params = get.urlencode()
-        url = reverse("moulinette_result")
-        url_with_params = f"{url}?{url_params}"
-        return HttpResponseRedirect(url_with_params)
 
+        moulinette_data = None
+        if self.request.method == "GET" and self.request.GET:
+            moulinette_data = self.request.GET
+        elif self.request.method in ("POST", "PUT"):
+            moulinette_data = self.request.POST
 
-class MoulinetteResult(TemplateView):
-    def get(self, request, *args, **kwargs):
-        try:
-            params = self.check_moulinette_params(request)
-        except (BadRequest, binascii.Error):
-            return HttpResponseRedirect(reverse("moulinette_home"))
+        if moulinette_data:
+            kwargs.update({"data": moulinette_data})
 
-        self.moulinette = Moulinette(params)
-        self.moulinette.run()
-        return super().get(request, *args, **kwargs)
-
-    def check_moulinette_params(self, request):
-        """Parse and validate the moulinette url parameters."""
-
-        data = request.GET.copy()
-        coords_b64 = data.get("coords", "e30=")  # "e30=" -> {}
-        coords = base64.urlsafe_b64decode(coords_b64).decode()
-        data["coords"] = coords
-
-        form = MoulinetteForm(data)
-        if not form.is_valid():
-            raise BadRequest("Invalid moulinette params")
-
-        return form.cleaned_data
-
-    def get_template_names(self):
-        moulinette_result = self.moulinette.eval_result
-        is_debug = bool(self.request.GET.get("debug", False))
-        if is_debug:
-            template_name = "moulinette/result_debug.html"
-        else:
-            template_name = f"moulinette/result_{moulinette_result}.html"
-        return [template_name]
+        return kwargs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["moulinette"] = self.moulinette
+
+        form = context["form"]
+        if form.is_valid():
+            moulinette = Moulinette(form.cleaned_data)
+            moulinette.run()
+            context["moulinette"] = moulinette
         return context
+
+    def render_to_response(self, context, **response_kwargs):
+        # We have to store the moulinette since there are no other way
+        # to give parameters to `get_template_names`
+        self.moulinette = context.get("moulinette", None)
+        return super().render_to_response(context, **response_kwargs)
+
+    def get_template_names(self):
+        """Check wich template to use depending on the moulinette result."""
+
+        moulinette = self.moulinette
+        is_debug = bool(self.request.GET.get("debug", False))
+
+        if moulinette is None:
+            template_name = "moulinette/home.html"
+        elif is_debug:
+            template_name = "moulinette/result_debug.html"
+        else:
+            moulinette_result = moulinette.eval_result
+            template_name = f"moulinette/result_{moulinette_result}.html"
+
+        return [template_name]
+
+    def form_valid(self, form):
+
+        get = QueryDict("", mutable=True)
+        form_data = form.cleaned_data
+        form_data.pop("address")
+        get.update(form_data)
+        url_params = get.urlencode()
+        url = reverse("moulinette_home")
+        url_with_params = f"{url}?{url_params}"
+        return HttpResponseRedirect(url_with_params)
