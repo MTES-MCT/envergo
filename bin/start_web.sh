c969d2dafa860899a12594da9d756e84cf0dd892
Thibault Jouannic <thibault@miximum.fr>
2022-03-16 11:20:53 +0100

    Add whoami info in gunicorn start script

--- bin/start_web.sh
+++ bin/start_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is ran by scalingo to start the application
 
-echo "Starting the Django app ($DJANGO_SETTINGS_MODULE)"
+echo "Starting the Django app ($DJANGO_SETTINGS_MODULE) as user `whoami`"
 
 gunicorn config.wsgi:application --log-file -
