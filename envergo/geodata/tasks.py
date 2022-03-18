d8a5f5a04a65fa8bda354d4792726817947a44ff
Thibault Jouannic <thibault@miximum.fr>
2022-03-18 15:18:25 +0100

    Update task status when it starts

--- envergo/geodata/tasks.py
+++ envergo/geodata/tasks.py
@@ -3,8 +3,8 @@ from envergo.geodata.models import Map
 from envergo.geodata.utils import extract_shapefile
 
 
-@app.task
-def process_shapefile_map(map_id):
+@app.task(bind=True)
+def process_shapefile_map(task, map_id):
     """Send a Mattermost notification to confirm the evaluation request."""
 
     map = Map.objects.get(pk=map_id)
