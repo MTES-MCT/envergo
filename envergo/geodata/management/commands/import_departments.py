23ab4b5920fda43c321796756df11e67aca06ba7
Thibault Jouannic <thibault@miximum.fr>
2022-03-28 11:41:49 +0200

    Add doc to the department import task

--- envergo/geodata/management/commands/import_departments.py
+++ envergo/geodata/management/commands/import_departments.py
@@ -5,7 +5,21 @@ from envergo.geodata.models import Department
 
 
 class Command(BaseCommand):
-    help = "Importe des zones à partir de shapefiles."
+    """Process departments from a shapefile.
+
+    Data source:
+    https://www.data.gouv.fr/fr/datasets/contours-des-departements-francais-issus-d-openstreetmap/
+
+    File url:
+    https://www.data.gouv.fr/fr/datasets/r/eb36371a-761d-44a8-93ec-3d728bec17ce
+
+    WARNING! This task is not meant to be called several times, since
+    metadata can be associated with Department objects in admin after they
+    where first imported.
+
+    """
+
+    help = "Importe les départments à partir d'un shapefile."
 
     def add_arguments(self, parser):
         parser.add_argument("shapefile", type=str)
