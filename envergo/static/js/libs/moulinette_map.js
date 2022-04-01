b0f7775c9a8407cb7c5763fd4905db2a57d36c30
Thibault Jouannic <thibault@miximum.fr>
2022-04-01 11:32:21 +0200

    Fix leaflet marker icon url generation issue

--- envergo/static/js/libs/moulinette_map.js
+++ envergo/static/js/libs/moulinette_map.js
@@ -47,6 +47,10 @@
    * Create the main marker object that is manipulated by the widget.
    */
   MoulinetteMap.prototype.initializeMarker = function(map) {
+    // Bypass an issue with leaflet detecting a bad icon url, caused by
+    // assets versioning
+    L.Icon.Default.prototype.options.imagePath = '/static/leaflet/images/';
+
     const options = {
       draggable: true,
       autoPan: true,
