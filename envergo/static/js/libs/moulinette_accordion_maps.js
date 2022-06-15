d2d291a138c11be9b07bebf777c8c32a3fac8263
Thibault Jouannic <thibault@miximum.fr>
2022-06-15 08:17:24 +0200

    Display map polygon

--- envergo/static/js/libs/moulinette_accordion_maps.js
+++ envergo/static/js/libs/moulinette_accordion_maps.js
@@ -18,9 +18,11 @@
   MapConfigurator.prototype.initMap = function(mapId) {
     const options = this.maps[mapId];
     const mapData = options.map;
-    const centerJson = mapData.center;
-    const center = [centerJson.coordinates[1], centerJson.coordinates[0]];
-    const map = L.map(options['divId'], { maxZoom: 21 }).setView(center, 17);
+    const center = mapData.center;
+
+    // Damn this constant lat and lng order mixing
+    const centerCoords = [center.coordinates[1], center.coordinates[0]];
+    const map = L.map(options['divId'], { maxZoom: 21 }).setView(centerCoords, 17);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 22,
@@ -31,8 +33,13 @@
     // Bypass an issue with leaflet detecting a bad icon url, caused by
     // assets versioning
     L.Icon.Default.prototype.options.imagePath = '/static/leaflet/images/';
-    const marker = L.marker(center);
+    const marker = L.marker(centerCoords);
     marker.addTo(map);
+
+    const polygon = mapData.polygon;
+    var polygonJson = L.geoJSON(polygon);
+    polygonJson.addTo(map);
+
     return map;
   };
 
