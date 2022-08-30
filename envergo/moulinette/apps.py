3c2eb7224b2974bb4d29b890d842a9289881d037
Thibault Jouannic <thibault@miximum.fr>
2022-08-30 11:02:35 +0200

    Rename "moulinette" admin module into "Simulateur"

--- envergo/moulinette/apps.py
+++ envergo/moulinette/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MoulinetteConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "envergo.moulinette"
+    verbose_name = "Simulateur"
