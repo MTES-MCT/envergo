2e9d13e2329b18ce5bd2736d64d3c17d1387be36
Thibault Jouannic <thibault@miximum.fr>
2022-09-13 10:02:09 +0200

    Don't log event when visitor cookie is disabled

--- envergo/analytics/utils.py
+++ envergo/analytics/utils.py
@@ -6,6 +6,8 @@ from envergo.analytics.models import Event
 def log_event(category, event, session, **kwargs):
 
     visitor_id = session.COOKIES.get(settings.VISITOR_COOKIE_NAME, "")
-    Event.objects.create(
-        category=category, event=event, session_key=visitor_id, metadata=kwargs
-    )
+
+    if visitor_id:
+        Event.objects.create(
+            category=category, event=event, session_key=visitor_id, metadata=kwargs
+        )
