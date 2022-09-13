a119533be332c93032fd0a0ce8c6233a8d0c8881
Thibault Jouannic <thibault@miximum.fr>
2022-09-13 15:38:20 +0200

    Write test for visitor cookie mechanism

---
+++ envergo/analytics/tests/test_middlewares.py
@@ -0,0 +1,34 @@
+import pytest
+from django.urls import reverse
+
+from envergo.analytics.middleware import SetVisitorIdCookie
+
+pytestmark = pytest.mark.django_db
+
+
+def test_set_visitor_id_cookie_middleware(client):
+    """A single page view sets the unique visitor cookie."""
+
+    res = client.get('/')
+    request = res.wsgi_request
+
+    assert 'visitorid' in request.COOKIES
+    assert request.COOKIES['visitorid']
+
+def test_visitor_cookie_deactivation(client):
+
+    # Set initial cookie
+    res = client.get('/')
+
+    disable_url = reverse('disable_visitor_cookie')
+    res = client.post(disable_url, data={}, follow=True)
+    request = res.wsgi_request
+
+    assert 'visitorid' in request.COOKIES
+    assert request.COOKIES['visitorid'] == ''
+
+    # New page views leave the cookie unset
+    res = client.get('/')
+    request = res.wsgi_request
+    assert 'visitorid' in request.COOKIES
+    assert request.COOKIES['visitorid'] == ''
