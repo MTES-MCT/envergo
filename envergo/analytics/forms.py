d0b8e9a5e37f42ec6dfeb7a65ec79dba741a4906
Thibault Jouannic <thibault@miximum.fr>
2022-09-14 16:44:52 +0200

    Initial feedback form modal display

---
+++ envergo/analytics/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+MSG_PLACEHOLDER = "Un élément qui manque de clarté, une information erronée, une proposition d'amélioration…"
+
+
+class FeedbackForm(forms.Form):
+    message = forms.CharField(
+        required=True,
+        label='Que souhaitez-vous nous demander ou signaler ?',
+        widget=forms.Textarea(attrs={'rows': 3, 'placeholder': MSG_PLACEHOLDER}))
+    contact = forms.CharField(
+        required=False,
+        label='Email ou téléphone',
+        help_text='Pour vous recontacter si vous souhaitez une réponse')
