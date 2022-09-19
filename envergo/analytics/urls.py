2aee721236a0831fb191cf70fb10a25324b09454
Thibault Jouannic <thibault@miximum.fr>
2022-09-19 10:06:00 +0200

    Remove useless `/` in url

--- envergo/analytics/urls.py
+++ envergo/analytics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from envergo.analytics.views import DisableVisitorCookie, FeedbackSubmit
 
 urlpatterns = [
-    path('/', FeedbackSubmit.as_view(), name='feedback_submit'),
+    path('', FeedbackSubmit.as_view(), name='feedback_submit'),
     path(
         "disablecookieview/",
         DisableVisitorCookie.as_view(),
