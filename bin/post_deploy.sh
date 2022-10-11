710f28ec32a91255a1d244d164d0ad2a4b9df95f
Thibault Jouannic <thibault@miximum.fr>
2022-10-11 11:27:08 +0200

    Ensure script errors make the scalingo build fail

--- bin/post_deploy.sh
+++ bin/post_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This script is ran by scalingo to start the application
 
+# Interrupt the script on error
+set -e
+
 echo ">>> Starting the post_deploy hook"
 
 python manage.py migrate
