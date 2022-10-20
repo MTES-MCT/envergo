f2d1b18421dc43ad73b345f9425a1e07d9d6a8cd
Thibault Jouannic <thibault@miximum.fr>
2022-10-20 11:25:03 +0200

    Copy url to clipboard on click

--- envergo/static/js/libs/moulinette_share_modal.js
+++ envergo/static/js/libs/moulinette_share_modal.js
@@ -1,17 +1,28 @@
-(function(exports, L) {
+(function(exports) {
   'use strict';
 
-  /**
-   * Initialize the leaflet maps for the moulinette result page.
-   */
   const ShareModal = function(dialogElt) {
     this.dialogElt = dialogElt;
+    this.urlInput = dialogElt.querySelector("input[type=url]");
+    this.shareBtn = dialogElt.querySelector("button[type=submit]");
   };
   exports.ShareModal = ShareModal;
 
-  ShareModal.prototype.init = function() {};
+  ShareModal.prototype.init = function() {
+    this.shareBtn.addEventListener('click', this.copyUrlToClipboard.bind(this));
+  };
 
-})(this, L);
+  ShareModal.prototype.copyUrlToClipboard = function() {
+    this.urlInput.focus();
+    this.urlInput.select();
+    document.execCommand("copy");
+
+    this.shareBtn.textContent = 'Le lien a bien été copié dans le presse-papier';
+    this.shareBtn.classList.add('fr-btn--icon-left');
+    this.shareBtn.classList.add('fr-icon-thumb-up-fill');
+  };
+
+})(this);
 
 window.addEventListener('load', function() {
   const dialogElt = document.getElementById(window.SHARE_MODAL_DIALOG_ID);
