69404a33f69c863dfef0923eb65162c69071154f
Thibault Jouannic <thibault@miximum.fr>
2023-02-28 15:36:40 +0100

    Filter polygon copy query

--- bin/copy_polygons.sh
+++ bin/copy_polygons.sh
@@ -16,7 +16,7 @@ dbclient-fetcher psql 13
 # Watch out! Don't empyt $PARENT_DATABASE_URL, it's the production database
 $HOME/bin/psql -d $DATABASE_URL -c "DELETE FROM geodata_zone;"
 
-nb_zones=$($HOME/bin/psql -d $PARENT_DATABASE_URL -t -c "SELECT COUNT(*) FROM geodata_zone;")
+nb_zones=$($HOME/bin/psql -d $PARENT_DATABASE_URL -t -c "SELECT COUNT(*) FROM geodata_zone z LEFT JOIN geodata_map m ON z.map_id = m.id WHERE m.copy_to_staging IS true;")
 page_size=1000
 nb_steps=$((nb_zones/page_size+1))
 
@@ -24,7 +24,7 @@ i=0
 while [ $i -lt $nb_steps ]; do
     echo ">>> Copying polygons from parent database: step $i / $nb_steps"
     offset=$((i*page_size))
-    query="COPY (SELECT * from geodata_zone ORDER BY id LIMIT $page_size OFFSET $offset) TO stdout"
+    query="COPY (SELECT z.* from geodata_zone z LEFT JOIN geodata_map m ON z.map_id = m.id WHERE m.copy_to_staging IS true ORDER BY z.id LIMIT $page_size OFFSET $offset) TO stdout"
     echo "$query"
     $HOME/bin/psql -d $PARENT_DATABASE_URL -c "$query" | $HOME/bin/psql -d $DATABASE_URL -c "COPY geodata_zone FROM stdin;"
 
