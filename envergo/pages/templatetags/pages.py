931ea144c9605ab1523fb87c2821719354be2fa4
Thibault Jouannic <thibault@miximum.fr>
2023-03-03 11:12:38 +0100

    Split the faq page into three pages

--- envergo/pages/templatetags/pages.py
+++ envergo/pages/templatetags/pages.py
@@ -52,6 +52,44 @@ def evalreq_menu(context):
 
 
 @register.simple_tag(takes_context=True)
+def faq_menu(context):
+    """Generate html for the "Faq" collapsible menu."""
+
+    try:
+        current_route = context.request.resolver_match.url_name
+    except AttributeError:
+        current_route = ""
+
+    links = (
+        ("faq_loi_sur_leau", "Loi sur l'eau"),
+        ("faq_natura_2000", "Natura 2000"),
+        ("faq_eval_env", "Évaluation environnementale"),
+    )
+    links_html = [nav_link(url, label, url == current_route) for url, label in links]
+
+    # urls for the menu items
+    routes = list(dict(links).keys())
+
+    # Other urls that can be reached from the menu
+    additional_routes = []
+    all_routes = routes + additional_routes
+
+    aria_current = 'aria-current="page"' if current_route in all_routes else ""
+    menu_html = f"""
+        <button class="fr-nav__btn" aria-expanded="false" aria-controls="menu-faq" {aria_current}>
+            Foire aux questions
+        </button>
+        <div class="fr-collapse fr-menu" id="menu-faq">
+          <ul class="fr-menu__list">
+            <li>
+            {'</li><li>'.join(links_html)}
+            </li>
+          </ul>
+        </div>
+    """
+    return mark_safe(menu_html)
+
+@register.simple_tag(takes_context=True)
 def evaluation_menu(context):
     """Generate html for the "Mes évaluations" collapsible menu."""
 
