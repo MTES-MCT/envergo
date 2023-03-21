aa9f260ee7e4ff81da339185e9783d99d96a58b5
Thibault Jouannic <thibault@miximum.fr>
2023-03-21 11:06:56 +0100

    Create new link checker page

--- envergo/pages/views.py
+++ envergo/pages/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.conf import settings
 from django.views.generic import TemplateView
 
@@ -18,3 +19,32 @@ class LegalMentionsView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["visitor_id"] = visitor_id
         return context
+
+
+class Outlinks(TemplateView):
+    template_name = "pages/outlinks.html"
+
+    def post(self, *args, **kwargs):
+        return self.get(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.POST:
+            links_report = self.check_links()
+            context["links"] = links_report
+        return context
+
+    def check_links(self):
+        token = settings.MATOMO_SECURITY_TOKEN
+        data_url = f"https://stats.data.gouv.fr/index.php?module=API&format=JSON&idSite=186&period=month&date=2023-03-10&method=Actions.getOutlinks&flat=1&token_auth={token}&filter_limit=100"
+        data = requests.get(data_url).json()
+
+        links = []
+        for datum in data:
+            url = datum["url"]
+            label = datum["label"]
+            req = requests.head(url)
+            links.append({"label": label, "url": url, "status": req.status_code})
+
+        return links
