1b03b9a744964f1f3fb1efc224cda81ef427cc43
Thibault Jouannic <thibault@miximum.fr>
2023-05-22 15:44:41 +0200

    Make the creation date updatable

--- envergo/pages/admin.py
+++ envergo/pages/admin.py
@@ -7,5 +7,4 @@ from envergo.pages.models import NewsItem
 class NewsItemAdmin(admin.ModelAdmin):
     list_display = ["title", "created_at"]
     search_fields = ["title", "content_md"]
-    fields = ["title", "content_md"]
-    readonly_fields = ["created_at"]
+    fields = ["title", "content_md", "created_at"]
