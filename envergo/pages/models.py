bd4761a126e5a5d0c8c1ad6100dd515a619ae8da
Thibault Jouannic <thibault@miximum.fr>
2023-05-22 10:07:20 +0200

    Add title to news items

--- envergo/pages/models.py
+++ envergo/pages/models.py
@@ -8,6 +8,7 @@ from envergo.utils.markdown import markdown_to_html
 class NewsItem(models.Model):
     """A news item to be displayed on the FAQ page."""
 
+    title = models.CharField(_("Title"), max_length=255)
     content_md = models.TextField(_("Content"))
     content_html = models.TextField(_("Content HTML"))
     created_at = models.DateTimeField(_("Date created"), default=timezone.now)
@@ -15,6 +16,9 @@ class NewsItem(models.Model):
     class Meta:
         verbose_name = _("News item")
         verbose_name_plural = _("News items")
+
+    def __str__(self):
+        return self.title
 
     def save(self, *args, **kwargs):
         self.content_html = markdown_to_html(self.content_md)
