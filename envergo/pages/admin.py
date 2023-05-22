659d6ba1d0e199f6cb0d86ef77a4fc9f5b29e5ed
Thibault Jouannic <thibault@miximum.fr>
2023-05-22 10:07:20 +0200

    Add title to news items

--- envergo/pages/admin.py
+++ envergo/pages/admin.py
@@ -5,7 +5,7 @@ from envergo.pages.models import NewsItem
 
 @admin.register(NewsItem)
 class NewsItemAdmin(admin.ModelAdmin):
-    list_display = ["created_at"]
-    search_fields = ["content_md"]
-    fields = ["content_md"]
+    list_display = ["title", "created_at"]
+    search_fields = ["title", "content_md"]
+    fields = ["title", "content_md"]
     readonly_fields = ["created_at"]
