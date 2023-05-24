1cfae60e251c476f81874bfee4196e61f7ef63b3
Thibault Jouannic <thibault@miximum.fr>
2023-05-24 10:25:24 +0200

    Fix broken tests

--- envergo/evaluations/tests/test_regulatory_reminder.py
+++ envergo/evaluations/tests/test_regulatory_reminder.py
@@ -13,46 +13,50 @@ def override_settings(settings):
     settings.DEFAULT_FROM_EMAIL = 'envergo-test@example.org'
 
 
-def test_instructor_to_field():
-    req = RequestFactory(user_type=USER_TYPES.instructor)
-    eval = req.create_evaluation()
-    email = eval.get_regulatory_reminder_email()
+def test_instructor_to_field(rf):
+    evalreq = RequestFactory(user_type=USER_TYPES.instructor)
+    eval = evalreq.create_evaluation()
+    req = rf.get("/")
+    email = eval.get_regulatory_reminder_email(req)
 
     assert email.to == ["instructor@example.org"]
 
 
-def test_petitioner_to_field():
-    req = RequestFactory(user_type=USER_TYPES.petitioner)
-    eval = req.create_evaluation()
-    email = eval.get_regulatory_reminder_email()
+def test_petitioner_to_field(rf):
+    evalreq = RequestFactory(user_type=USER_TYPES.petitioner)
+    eval = evalreq.create_evaluation()
+    req = rf.get("/")
+    email = eval.get_regulatory_reminder_email(req)
 
     assert email.to == ["sponsor1@example.org", "sponsor2@example.org"]
 
 
-def test_instructor_cc_field():
-    req = RequestFactory(user_type=USER_TYPES.instructor, send_eval_to_sponsor=True)
-    eval = req.create_evaluation()
-    email = eval.get_regulatory_reminder_email()
+def test_instructor_cc_field(rf):
+    evalreq = RequestFactory(user_type=USER_TYPES.instructor, send_eval_to_sponsor=True)
+    eval = evalreq.create_evaluation()
+    req = rf.get("/")
+    email = eval.get_regulatory_reminder_email(req)
 
     assert email.cc == ["sponsor1@example.org", "sponsor2@example.org"]
 
-    req = RequestFactory(user_type=USER_TYPES.instructor, send_eval_to_sponsor=False)
-    eval = req.create_evaluation()
-    email = eval.get_regulatory_reminder_email()
+    evalreq = RequestFactory(user_type=USER_TYPES.instructor, send_eval_to_sponsor=False)
+    eval = evalreq.create_evaluation()
+    email = eval.get_regulatory_reminder_email(req)
 
     assert email.cc == []
 
 
-def test_bcc_field(loire_atlantique_department):  # noqa
-    req = RequestFactory()
-    eval = req.create_evaluation()
-    email = eval.get_regulatory_reminder_email()
+def test_bcc_field(rf, loire_atlantique_department):  # noqa
+    evalreq = RequestFactory()
+    eval = evalreq.create_evaluation()
+    req = rf.get("/")
+    email = eval.get_regulatory_reminder_email(req)
     # Default bcc field is our own address
     assert email.bcc == ['envergo-test@example.org']
 
     MoulinetteConfigFactory(department=loire_atlantique_department, ddtm_contact_email='ddtm_email_test@example.org')
     url = "https://example.org?lng=-2.08555&lat=47.31051"  # Somewhere in Loire-Atlantique (44), France
-    req = RequestFactory(moulinette_url=url)
-    eval = req.create_evaluation()
-    email = eval.get_regulatory_reminder_email()
+    evalreq = RequestFactory(moulinette_url=url)
+    eval = evalreq.create_evaluation()
+    email = eval.get_regulatory_reminder_email(req)
     assert email.bcc == ['envergo-test@example.org', "ddtm_email_test@example.org"]
