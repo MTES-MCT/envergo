cb1726b2fffe281d8e2a7e8153a1d3cbdd886e43
Thibault Jouannic <thibault@miximum.fr>
2023-06-15 10:51:02 +0200

    Update map zone count and import status

--- envergo/geodata/management/commands/update_zone_counts.py
+++ envergo/geodata/management/commands/update_zone_counts.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
-from django.db.models import Count, OuterRef, Subquery
+from django.db.models import Case, Count, F, OuterRef, Subquery, Value, When
 
-from envergo.geodata.models import Map, Zone
+from envergo.geodata.models import STATUSES, Map, Zone
 
 
 class Command(BaseCommand):
@@ -14,4 +14,11 @@ class Command(BaseCommand):
             .annotate(count=Count("id"))
             .values("count")
         )
-        Map.objects.update(imported_zones=zone_count)
+        Map.objects.annotate(nb_zones=zone_count).update(
+            imported_zones=F("nb_zones"),
+            import_status=Case(
+                When(nb_zones=F("expected_zones"), then=Value(STATUSES.success)),
+                When(nb_zones__gt=0, then=Value(STATUSES.partial_success)),
+                default=Value(STATUSES.failure),
+            ),
+        )
