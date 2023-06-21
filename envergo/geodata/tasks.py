21d79034d977957e5c8cbff03ce90421d8259541
Thibault Jouannic <thibault@miximum.fr>
2023-06-21 10:14:49 +0200

    Better map import transaction management

--- envergo/geodata/tasks.py
+++ envergo/geodata/tasks.py
@@ -10,23 +10,30 @@ logger = logging.getLogger(__name__)
 
 
 @app.task(bind=True)
+@transaction.atomic
 def process_shapefile_map(task, map_id):
     logger.info(f"Starting import on map {map_id}")
 
     map = Map.objects.get(pk=map_id)
+
+    # Store the task data in the model, so we can display progression
+    # in the admin page.
     map.task_id = task.request.id
     map.import_error_msg = ""
     map.import_status = None
     map.save()
 
-    map.zones.all().delete()
+    # Proceed with the map import
     try:
         with transaction.atomic():
+            map.zones.all().delete()
             process_shapefile(map, map.file, task)
+            map.geometry = simplify_map(map)
     except Exception as e:
         map.import_error_msg = f"Erreur d'import ({e})"
         logger.error(map.import_error_msg)
 
+    # Update the map status and metadata
     nb_imported_zones = map.zones.all().count()
     if map.expected_zones == nb_imported_zones:
         map.import_status = STATUSES.success
@@ -37,5 +44,4 @@ def process_shapefile_map(task, map_id):
 
     map.task_id = None
     map.imported_zones = nb_imported_zones
-    map.geometry = simplify_map(map)
     map.save()
