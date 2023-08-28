6693d0cb068e50520414ba2a8b6a57f703b68f0f
Thibault Jouannic <thibault@miximum.fr>
2023-08-28 14:57:41 +0200

    Redirect urls

--- envergo/evaluations/redirect_urls.py
+++ envergo/evaluations/redirect_urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/evaluations", permanent=True)),
-    path(
-        "<path:url>", RedirectView.as_view(url="/evaluations/%(url)s", permanent=True)
-    ),
+    path("", RedirectView.as_view(url="/avis", permanent=True)),
+    path("<path:url>", RedirectView.as_view(url="/avis/%(url)s", permanent=True)),
 ]
