ed6fa524da2cf25c584f8db536dca39324b40be1
Thibault Jouannic <thibault@miximum.fr>
2023-09-06 10:35:16 +0200

    Log all esp events

--- envergo/evaluations/signals.py
+++ envergo/evaluations/signals.py
@@ -11,6 +11,8 @@ logger = logging.getLogger(__name__)
 
 @receiver(tracking)
 def handle_mail_event(sender, event, esp_name, **kwargs):
+    logger.info(f"Received event from {esp_name}: {event}")
+
     event_name = event["event"]
     recipient = event["email"]
     timestamp = event["ts"]
