afce1567fe58f85d41e6424bacc47d8a283a8cac
Thibault Jouannic <thibault@miximum.fr>
2023-09-12 15:35:33 +0200

    Disable the form button upon submission

---
+++ envergo/static/js/libs/form_submit_feedback.js
@@ -0,0 +1,31 @@
+// Disables the form submit button upon submission
+(function (exports) {
+  'use strict';
+
+  const SimulationForm = function (formElt) {
+    this.formElt = formElt;
+    this.buttonElt = formElt.querySelector('button[type=submit]');
+  };
+  exports.SimulationForm = SimulationForm;
+
+  SimulationForm.prototype.init = function () {
+    this.formElt.addEventListener('submit', this.onFormSubmit.bind(this));
+  };
+
+  SimulationForm.prototype.onFormSubmit = function (evt) {
+    this.buttonElt.disabled = true;
+    this.buttonElt.classList.add("fr-mr-1w");
+
+    let textElt = document.createElement('span');
+    textElt.innerHTML = 'Évaluation en cours…';
+    textElt.style.display = "inline";
+    textElt.classList.add("fr-hint-text");
+    this.buttonElt.insertAdjacentElement("afterend", textElt);
+  };
+
+})(this);
+
+window.addEventListener('load', function () {
+  const form = document.getElementById(SIMULATION_FORM_ID);
+  new SimulationForm(form).init();
+});
