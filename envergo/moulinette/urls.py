8bfb0cbe84121df418fcffd15ca0da129f0fca63
Thibault Jouannic <thibault@miximum.fr>
2023-10-02 15:06:32 +0200

    Track "missing data" page as a custom url

--- envergo/moulinette/urls.py
+++ envergo/moulinette/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
+from django.views.generic import RedirectView
 
 from envergo.moulinette.views import (
     MoulinetteDebug,
@@ -15,6 +16,14 @@ urlpatterns = [
         include(
             [
                 path("", MoulinetteResult.as_view(), name="moulinette_result"),
+                # We need this url to exist, but it's a "fake" url, it's only
+                # used to be logged in matomo, so we can correctry track the funnel
+                # moulinette home > missing data > final result
+                path(
+                    _("missing-data/"),
+                    RedirectView.as_view(pattern_name="moulinette_result"),
+                    name="moulinette_missing_data",
+                ),
                 path(
                     "<slug:regulation>/",
                     MoulinetteRegulationResult.as_view(),
