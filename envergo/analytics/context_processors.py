cf50a1340e5cb013e00c9be1223a95e293f99ee6
Thibault Jouannic <thibault@miximum.fr>
2023-10-04 09:33:39 +0200

    Refactor code by splitting into custom methods

--- envergo/analytics/context_processors.py
+++ envergo/analytics/context_processors.py
@@ -6,21 +6,8 @@ from config.settings.base import VISITOR_COOKIE_NAME
 from envergo.analytics.models import Event
 
 
-def analytics(request):
-    visitor_id = request.COOKIES[VISITOR_COOKIE_NAME]
-
+def build_usage_facet(usage_data):
     now = timezone.now()
-    q_request = Q(category="evaluation") & Q(event="request")
-    q_simulation = Q(category="simulateur") & Q(event="soumission")
-    events = (
-        Event.objects.filter(session_key=visitor_id)
-        .filter(q_request | q_simulation)
-        .values("category", "event")
-        .annotate(count=Count("*"), latest_date=Max("date_created"))
-        .order_by("event")
-    )
-
-    usage_data = events[0]
     usage_delta = (now - usage_data["latest_date"]).days
 
     if usage_data["count"] >= 2:
@@ -36,8 +23,11 @@ def analytics(request):
         usage_last = "d1"
 
     usage_facet = f"{usage_nb}_{usage_last}"
+    return usage_facet
 
-    simulation_data = events[1]
+
+def build_simulation_facet(simulation_data):
+    now = timezone.now()
     simulation_delta = (now - simulation_data["latest_date"]).days
 
     if simulation_data["count"] >= 10:
@@ -55,7 +45,31 @@ def analytics(request):
         simulation_last = "d1"
 
     simulation_facet = f"{simulation_nb}_{simulation_last}"
+    return simulation_facet
 
+
+def analytics(request):
+    """Add some usage analytics to the templates context.
+
+    We want to segment our Matomo analytics with different kind of users.
+
+    We use matomo custom dimensions, but instead of storing raw data, it
+    was decided that we manually build the facets here."""
+
+    visitor_id = request.COOKIES[VISITOR_COOKIE_NAME]
+
+    q_request = Q(category="evaluation") & Q(event="request")
+    q_simulation = Q(category="simulateur") & Q(event="soumission")
+    events = (
+        Event.objects.filter(session_key=visitor_id)
+        .filter(q_request | q_simulation)
+        .values("category", "event")
+        .annotate(count=Count("*"), latest_date=Max("date_created"))
+        .order_by("event")
+    )
+
+    usage_facet = build_usage_facet(events[0])
+    simulation_facet = build_simulation_facet(events[1])
     matomo_dimensions = [
         (settings.MATOMO_USAGE_DIMENSION_ID, usage_facet),
         (settings.MATOMO_SIMULATION_DIMENSION_ID, simulation_facet),
