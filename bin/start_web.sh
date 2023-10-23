1112f03818c4f61b71497c0c02e582d83a6e4af0
Thibault Jouannic <thibault@miximum.fr>
2023-10-23 11:27:13 +0200

    Test perf improvement gunicorn settings

--- bin/start_web.sh
+++ bin/start_web.sh
@@ -3,4 +3,4 @@
 
 echo "Starting the Django app ($DJANGO_SETTINGS_MODULE) as user `whoami`"
 
-gunicorn config.wsgi:application --log-file -
+gunicorn config.wsgi:application --preload --max-requests 600 --log-file -
