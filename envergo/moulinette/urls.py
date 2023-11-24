11d8f5774516164a04516a62fff814bd08a731cd
Thibault Jouannic <thibault@miximum.fr>
2023-11-24 11:01:47 +0100

    Setup moulinette and home page redirections

--- envergo/moulinette/urls.py
+++ envergo/moulinette/urls.py
@@ -10,7 +10,21 @@ from envergo.moulinette.views import (
 )
 
 urlpatterns = [
-    path("", MoulinetteHome.as_view(), name="moulinette_home"),
+    # Redirections history
+    # For a long time, there was no homepage to EnvErgo, so the home url (/) just redirected
+    # to the moulinette form (/simulateur), which showed a bit of context about the site.
+    # When we designed the home page, the /simulateur url was cleaned and only shows
+    # the form.
+    # Problem is that many external links or saved urls pointed to the /simulateur/ url,
+    # and we feared many people would be lost by discovering EnvErgo throught the form
+    # url instead of the real home page. Thus we decided to redirect the /simulateur/ url
+    # to the home page.
+    path(
+        "",
+        RedirectView.as_view(pattern_name="home", query_string=True),
+        name="moulinette_home_redirect",
+    ),
+    path(_("form/"), MoulinetteHome.as_view(), name="moulinette_home"),
     path(
         _("result/"),
         include(
