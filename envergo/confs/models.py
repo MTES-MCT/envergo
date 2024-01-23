9c82646a520bc41b81f34c17228e9e03d43243d7
Thibault Jouannic <thibault@miximum.fr>
2024-01-23 09:49:52 +0100

    Create model to store settings in admin

--- envergo/confs/models.py
+++ envergo/confs/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
 from envergo.utils.markdown import markdown_to_html
 
 
 class TopBar(models.Model):
-    """A singleton object to configure the top bar in admin."""
+    """An object to configure the main message top bars."""
 
     message_md = models.TextField(_("Message"))
     message_html = models.TextField(_("Message (html)"), blank=True)
@@ -21,3 +22,24 @@ class TopBar(models.Model):
         striped = paragraph.removeprefix("<p>").removesuffix("</p>")
         self.message_html = striped
         super().save(*args, **kwargs)
+
+
+SETTINGS = Choices(
+    (
+        "evalreq_confirmation_email_delay_mention",
+        "E-mail de confirmation de demande d'éval., mention de la délai de réponse",
+    ),
+)
+
+
+class Setting(models.Model):
+    """A single option."""
+
+    setting = models.CharField(
+        _("Setting"), max_length=256, choices=SETTINGS, unique=True
+    )
+    value = models.TextField(_("Value"), max_length=256)
+
+    class Meta:
+        verbose_name = _("Setting")
+        verbose_name_plural = _("Settings")
