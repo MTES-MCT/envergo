ee99bb951321d2f9c6138ce78dda38b0a75cda92
Thibault Jouannic <thibault@miximum.fr>
2024-01-23 09:49:52 +0100

    Create model to store settings in admin

--- envergo/confs/admin.py
+++ envergo/confs/admin.py
@@ -3,7 +3,7 @@ from textwrap import shorten
 from django import forms
 from django.contrib import admin
 
-from envergo.confs.models import TopBar
+from envergo.confs.models import Setting, TopBar
 
 
 class TopBarAdminForm(forms.ModelForm):
@@ -31,3 +31,9 @@ class TopBarAdmin(admin.ModelAdmin):
 
     def message_summary(self, obj):
         return shorten(obj.message_md, width=50, placeholder="…")
+
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ["setting", "value"]
+    fields = ("setting", "value")
