82bd3d54247d0ebb7b2aaba5c4e1fcc2622f86a9
Thibault Jouannic <thibault@miximum.fr>
2024-02-26 16:18:30 +0100

    Display pixels on the map

--- envergo/static/js/libs/2150_map.js
+++ envergo/static/js/libs/2150_map.js
@@ -1,6 +1,15 @@
 (function (exports, L) {
   'use strict';
 
+  var getColor = function (d) {
+    // Thanks to ColorBrewer for the color scale
+    // https://colorbrewer2.org/#type=sequential&scheme=RdPu&n=9
+    let scale = ["#00000000", "#fff7f3", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#dd3497", "#ae017e", "#7a0177", "#49006a"];
+    let d_clamped = Math.max(0, Math.min(d, 12000));
+    let color_index = Math.floor(d_clamped / 12000 * (scale.length - 1));
+    return scale[color_index];
+  }
+
   /**
    * Settings and behavior for the moulinette form map widget.
    */
@@ -9,6 +18,9 @@
     this.configureLeaflet();
     this.map = this.initializeMap();
     this.marker = this.initializeMarker();
+    this.drawPolygons();
+    this.addLegend();
+    this.addControl();
 
     if (this.options.displayMarker) {
       this.marker.addTo(this.map);
@@ -48,7 +60,7 @@
       "&TILEROW={y}" +
       "&TILECOL={x}", {
       maxZoom: 22,
-      maxNativeZoom: 19,
+      maxNativeZoom: 16,
       tileSize: 256,
       attribution: '&copy; <a href="https://www.ign.fr/">IGN</a>'
     }).addTo(map);
@@ -71,6 +83,109 @@
 
     const marker = L.marker(this.options.centerMap, options);
     return marker;
+  };
+
+  Map.prototype.drawPolygons = function () {
+
+
+    var style = function (polygon) {
+      return {
+        fillColor: getColor(polygon.properties.value),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+    };
+
+    var onEachFeature = function (feature, layer) {
+      layer.on({
+        mouseover: this.highlightFeature.bind(this),
+        mouseout: this.resetHighlight.bind(this),
+      });
+    };
+
+    if (this.options.polygons) {
+      var features = this.options.polygons.map(function (polygon) {
+        var polygonJSON = {
+          type: "Feature",
+          properties: { value: polygon[2] },
+          geometry: JSON.parse(polygon[3])
+        };
+        return polygonJSON;
+      });
+
+      var geoJSON = L.geoJSON(
+        {
+          type: "FeatureCollection",
+          features: features,
+        },
+        { style: style, onEachFeature: onEachFeature.bind(this) });
+
+      geoJSON.addTo(this.map);
+    }
+  };
+
+  Map.prototype.highlightFeature = function (e) {
+
+    var layer = e.target;
+    layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
+    layer.bringToFront();
+    this.info.update(layer.feature.properties);
+  };
+
+  Map.prototype.resetHighlight = function (e) {
+    var layer = e.target;
+    layer.setStyle({
+      weight: 2,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    });
+    this.info.update();
+  };
+
+  Map.prototype.addLegend = function () {
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend');
+      var grades = [0, 2000, 4000, 6000, 8000, 10000, 12000];
+      var labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+      return div;
+    };
+    legend.addTo(this.map);
+  };
+
+  Map.prototype.addControl = function () {
+    this.info = L.control();
+
+    this.info.onAdd = function (map) {
+      this._div = L.DomUtil.create('div', 'info');
+      this.update();
+      return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    this.info.update = function (props) {
+      this._div.innerHTML = '<strong>Surface de bassin versant</strong><br />' + (props ?
+        Math.floor(props.value) + ' m<sup>2</sup>'
+        : '');
+    };
+
+    this.info.addTo(this.map);
   };
 
   Map.prototype.setMarkerPosition = function (latLng, zoomLevel) {
@@ -142,6 +257,7 @@
       defaultZoom: DEFAULT_ZOOM,
       latFieldId: LAT_FIELD_ID,
       lngFieldId: LNG_FIELD_ID,
+      polygons: POLYGONS,
     }
     map = new Map(options);
   });
