2f33e3e9ed64aa1335bdd4625edb446d95aac9aa
Thibault Jouannic <thibault@miximum.fr>
2024-02-28 10:47:32 +0100

    Use an interpolation to get a more precise value

--- envergo/geodata/views.py
+++ envergo/geodata/views.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 import requests
-from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.geos import GEOSGeometry, Point
 from django.core.serializers import serialize
 from django.db import connection
 from django.http import JsonResponse
@@ -10,6 +10,7 @@ from django.http.response import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView, TemplateView, View
+from scipy.interpolate import griddata
 from shapely.geometry import mapping, shape
 from shapely.ops import unary_union
 
@@ -17,6 +18,10 @@ from envergo.geodata.forms import LatLngForm
 from envergo.geodata.models import Zone
 
 logger = logging.getLogger(__name__)
+
+
+EPSG_WGS84 = 4326
+EPSG_LAMB93 = 2154
 
 
 class ParcelsExport(View):
@@ -84,29 +89,98 @@ class CatchmentAreaDebug(FormView):
         if form.is_bound and "lng" in form.cleaned_data and "lat" in form.cleaned_data:
             lng, lat = form.cleaned_data["lng"], form.cleaned_data["lat"]
 
-            # EPSG_WGS84 = 4326
-            # EPSG_MERCATOR = 3857
-            # lng_lat = Point(float(lng), float(lat), srid=EPSG_WGS84)
-            # coords = lng_lat.transform(EPSG_MERCATOR, clone=True)
-            # tiles = CatchmentAreaTile.objects.filter(data__contains=coords)
+            lng_lat = Point(float(lng), float(lat), srid=EPSG_WGS84)
+            lamp93_coords = lng_lat.transform(EPSG_LAMB93, clone=True)
 
             with connection.cursor() as cursor:
+                # It took me a week to come up with the following queries, so here is
+                # a bit of explanation.
+
+                # In the database, we have a raster storing catchment area values for
+                # various coordinates, arranged in a 20x20m grid, and stored in a
+                # Lambert93 projection.
+                # The user provides a lat/lng coordinate, and we want to know the
+                # catchment area at this point.
+
+                # Here is the catch: we don't just want to get the nearest value, since
+                # there can be huge variations from one cell to the other.
+                # So we have to use bilinear interpolaton to "smooth" the values.
+
+                # I couldn't find a way to do this directly in PostGIS, so the actual
+                # interpolation has to be performed in Python. It means we need to
+                # fetch a grid of coordinates / values around the point from the db.
+
+                # The usual raster querying methods ST_Value, ST_NearestValue and
+                # ST_Neighborhood return value from the raster, but not the coordinates.
+                # So we have to use the alternative ST_PixelAsPoints, which converts
+                # the raster values into Point geometries, alongside the associated values.
+
+                # To only get the relevant values, we clip the raster with a bounding box
+                # around our point using ST_Clip(ST_Envelope(ST_Buffer(…
+
                 query = """
-                SELECT ST_Neighborhood(tiles.data, point, 1, 1)
-                FROM geodata_catchmentareatile AS tiles
-                CROSS JOIN ST_Transform(ST_Point(%s, %s, 4326), 2154) AS point
-                WHERE ST_Intersects(tiles.data, point)
+                SELECT ST_X(geom), ST_Y(geom), val
+                FROM (
+                  SELECT
+                    (ST_PixelAsPoints(
+                      ST_Clip(
+                        tiles.data,
+                        ST_Envelope(
+                          ST_Buffer(point, 30)
+                        )
+                      )
+                  )).*
+                  FROM
+                    geodata_catchmentareatile AS tiles
+                    CROSS JOIN
+                      ST_Transform(
+                        ST_Point(%s, %s, 4326),
+                        2154
+                    ) AS point
+                  WHERE
+                    ST_Intersects(tiles.data, point)
+                  ) points;
                 """
                 cursor.execute(query, [lng, lat])
-                row = cursor.fetchone()
-                areas = row[0]
-                catchment_area = int(areas[1][1])
+                pixels = cursor.fetchall()
+                coords = [(x, y) for x, y, v in pixels]
+                values = [v for x, y, v in pixels]
+                interpolated_area = griddata(
+                    coords, values, lamp93_coords, method="cubic"
+                )[0]
+
+                # The value we display is actually rounded to the nearest 500m²
+                catchment_area = int(interpolated_area)
                 catchment_area_500 = round(catchment_area / 500) * 500
+
+                # Compute values relevant to the moulinette result
                 value_action_requise = max(0, 7000 - catchment_area_500)
                 value_soumis = max(0, 12000 - catchment_area_500)
 
+                # This next query only exists to gather data for the map display.
+                # We want to display an interactive grid on the map, with a colored based
+                # legend.
+
+                # PostGIS provides the handsy ST_PixelAsPolygons, which returns a
+                # list of square polygons, each one representing a pixel of the raster.
+                # The only subtlety is that for each cell, the actual data points
+                # correspond to the top left corner, which is not very intuitive in a
+                # visualization. Thus, we have to shift our entire grid so that each cell
+                # is centered on the corresponding point.
                 query = """
-                SELECT (gv).x, (gv).y, (gv).val, ST_AsGeoJSON(ST_Transform((gv).geom, 4326)) geom
+                SELECT
+                  (gv).x,
+                  (gv).y,
+                  (gv).val,
+                  ST_AsGeoJSON(
+                    ST_Transform(
+                      ST_Translate(
+                        (gv).geom,
+                        -10, 10
+                      ),
+                      4326
+                    )
+                  ) geom
                 FROM
                   (
                     SELECT
@@ -115,15 +189,19 @@ class CatchmentAreaDebug(FormView):
                           ST_Clip(
                             tiles.data,
                             ST_Envelope(
-                              ST_Buffer(point, 50)
+                              ST_Buffer(
+                                ST_Translate(point, 10, -10),
+                                50
+                              )
                             ),
                             false
                           )
                         )
                       ).*
                     FROM
-                      geodata_catchmentareatile AS tiles CROSS
-                      JOIN ST_Transform(
+                      geodata_catchmentareatile AS tiles
+                    CROSS JOIN
+                      ST_Transform(
                         ST_Point(%s, %s, 4326),
                         2154
                       ) AS point
@@ -135,9 +213,9 @@ class CatchmentAreaDebug(FormView):
                 polygons = cursor.fetchall()
 
                 context["result_available"] = True
-                context["areas"] = areas
                 context["catchment_area"] = catchment_area
                 context["catchment_area_500"] = catchment_area_500
+                context["interpolated_area"] = interpolated_area
                 context["value_action_requise"] = value_action_requise
                 context["value_soumis"] = value_soumis
                 context["polygons"] = json.dumps(polygons)
