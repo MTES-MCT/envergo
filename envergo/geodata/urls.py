97b843498cbab4849f0eba123bc43c0467f972c4
Thibault Jouannic <thibault@miximum.fr>
2024-03-04 09:29:03 +0100

    Update 2150 debug page url

--- envergo/geodata/urls.py
+++ envergo/geodata/urls.py
@@ -5,7 +5,7 @@ from envergo.geodata.views import CatchmentAreaDebug, ZoneMap, ZoneSearch
 
 urlpatterns = [
     path(_("map/"), ZoneMap.as_view(), name="zone_map"),
-    path("2150/", CatchmentAreaDebug.as_view(), name="2150_debug"),
+    path("demonstrateur-bv/", CatchmentAreaDebug.as_view(), name="2150_debug"),
     path(
         _("zone.geojson"),
         ZoneSearch.as_view(),
