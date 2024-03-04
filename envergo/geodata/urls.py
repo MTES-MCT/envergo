7ecf9e02d501168593f022bbcad929888b1ac0a4
Thibault Jouannic <thibault@miximum.fr>
2024-03-04 15:23:14 +0100

    Update urls

--- envergo/geodata/urls.py
+++ envergo/geodata/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from django.utils.translation import gettext_lazy as _
 
-from envergo.geodata.views import CatchmentAreaDebug, ZoneMap, ZoneSearch
+from envergo.geodata.views import ZoneMap, ZoneSearch
 
 urlpatterns = [
     path(_("map/"), ZoneMap.as_view(), name="zone_map"),
-    path("demonstrateur-bv/", CatchmentAreaDebug.as_view(), name="2150_debug"),
     path(
         _("zone.geojson"),
         ZoneSearch.as_view(),
