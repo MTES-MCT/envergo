9c2bdd372998f7525aeed9c513e5ab7a930d85ee
Thibault Jouannic <thibault@miximum.fr>
2024-04-25 11:34:03 +0200

    Add ability to edit user groups

--- envergo/users/admin.py
+++ envergo/users/admin.py
@@ -22,6 +22,7 @@ class UserAdmin(auth_admin.UserAdmin):
                     "is_active",
                     "is_staff",
                     "is_superuser",
+                    "groups",
                 ),
             },
         ),
