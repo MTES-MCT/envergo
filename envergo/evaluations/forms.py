7f65ec2ced7d5e93986280c8ce4b748e2aa14e28
Thibault Jouannic <thibault@miximum.fr>
2024-05-27 15:00:28 +0200

    Fix an issue with conditional field validation

--- envergo/evaluations/forms.py
+++ envergo/evaluations/forms.py
@@ -41,6 +41,7 @@ class EvaluationFormMixin(forms.Form):
             send_eval_to_project_owner = data.get("send_eval_to_project_owner", False)
             if not send_eval_to_project_owner:
                 self.fields["project_owner_emails"].required = False
+                self.fields["project_owner_phone"].required = False
 
         return super().full_clean()
 
