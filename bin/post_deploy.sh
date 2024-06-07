46f812c1a778c4181b048e4bd91857b78e9a5474
PY <py.dezaunay@gmail.com>
2024-06-07 12:04:43 +0200

    let's try

--- bin/post_deploy.sh
+++ bin/post_deploy.sh
@@ -8,17 +8,7 @@ echo ">>> Starting the post_deploy hook"
 
 python manage.py migrate
 
-# WIP to remove
-echo $APP
-echo $PORT
-echo $CONTAINER
-echo $CONTAINER_VERSION
-echo $CONTAINER_SIZE
-echo $CONTAINER_MEMORY
-echo $HOSTNAME
-echo $STACK
-echo $REGION_NAME
+python manage.py deploy_environment $APP.$REGION_NAME.scalingo.io
 
-python manage.py deploy_environment $APP
 
 echo ">>> Leaving the post_deploy hook"
