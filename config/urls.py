af439ce90deec4017e23e69559daa4331afdf09c
PY <py.dezaunay@gmail.com>
2024-07-23 10:39:51 +0200

    use dynamic template for each common view

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import SetPasswordForm
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
 from django.views import defaults as default_views
+
+from envergo.users import views as auth_views
 
 # We redefine django auth patterns for better customization
 auth_patterns = [
