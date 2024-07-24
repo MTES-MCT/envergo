457e7d525058b25c8a8dcd2efed37a5b43fed686
PY <py.dezaunay@gmail.com>
2024-07-24 11:23:03 +0200

    fix

--- envergo/contrib/sites/tests/factories.py
+++ envergo/contrib/sites/tests/factories.py
@@ -1,11 +1,10 @@
+from django.contrib.sites.models import Site
 from factory.django import DjangoModelFactory
-
-from envergo.evaluations.models import Evaluation
 
 
 class SiteFactory(DjangoModelFactory):
     class Meta:
-        model = Evaluation
+        model = Site
 
     domain = "testserver"
     name = "testserver"
