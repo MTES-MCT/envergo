928632688ae0fd6f7bbe0892de01601082ac81dc
PY <py.dezaunay@gmail.com>
2024-07-24 06:19:40 +0200

    Settings are Read only + migration + add fixture

--- envergo/conftest.py
+++ envergo/conftest.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 import pytest
+from django.contrib.sites.models import Site
 
 from envergo.users.models import User
 from envergo.users.tests.factories import UserFactory
@@ -17,3 +20,17 @@ def user() -> User:
 @pytest.fixture
 def admin_user() -> User:
     return UserFactory(is_staff=True, is_superuser=True)
+
+
+@pytest.fixture(autouse=True)
+def mock_get_current_site():
+    # Create a mock site
+    mock_site = Site()
+    mock_site.domain = "www.example.com"
+    mock_site.name = "example"
+
+    # Use patch to replace get_current_site with your mock
+    with patch(
+        "django.contrib.sites.shortcuts.get_current_site", return_value=mock_site
+    ):
+        yield
