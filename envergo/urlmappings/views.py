9636d9905cf5a361b6430bbcc5efb8d127d13e05
Thibault Jouannic <thibault@miximum.fr>
2024-07-26 10:00:46 +0200

    Create initial mapping creation view

--- envergo/urlmappings/views.py
+++ envergo/urlmappings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
+from django_ratelimit.decorators import ratelimit
 
-# Create your views here.
+from envergo.urlmappings.forms import UrlMappingCreateForm
+
+
+@method_decorator(ratelimit(key="ip", rate="10/m", method="POST"), name="post")
+class UrlMappingCreateView(CreateView):
+    http_method_names = ["post"]
+    form_class = UrlMappingCreateForm
+
+    def form_valid(self, form):
+        mapping = form.save()
+        short_url = self.request.build_absolute_uri(f"/r/{mapping.key}/")
+        return JsonResponse(
+            {"short_url": short_url, "key": mapping.key, "url": mapping.url}, status=201
+        )
