9fd123db48e32c3f24f645e4b918ccf32f4c8cae
Thibault Jouannic <thibault@miximum.fr>
2024-07-26 09:31:07 +0200

    Initialize urlmappings app

---
+++ envergo/urlmappings/models.py
@@ -0,0 +1,25 @@
+import secrets
+
+from django.conf import settings
+from django.db import models
+
+
+def generate_key():
+    """Generate a short random and readable key."""
+
+    # letters and numbers without l, 1, i, O, 0, etc.
+    alphabet = "abcdefghjkmnpqrstuvwxyz23456789"
+    length = settings.URLMAPPING_KEY_LENGTH
+    key = "".join(secrets.choice(alphabet) for i in range(length))
+
+    return key
+
+
+class Mapping(models.Model):
+    """A mapping between a short key and a URL."""
+
+    key = models.CharField(max_length=20, unique=True, default=generate_key)
+    url = models.URLField()
+
+    def __str__(self):
+        return f"{self.key}"
