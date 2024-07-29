498359cd12ddfc16de7eb52955a136ed1e4279cd
Thibault Jouannic <thibault@miximum.fr>
2024-07-29 10:34:24 +0200

    Fix typo in admin module registering method

--- envergo/urlmappings/admin.py
+++ envergo/urlmappings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from envergo.urlmappings.models import UrlMapping
 
 
-@admin.site.register(UrlMapping)
+@admin.register(UrlMapping)
 class UrlMappingAdmin(admin.ModelAdmin):
     list_display = ("key", "url")
     search_fields = ("key", "url")
