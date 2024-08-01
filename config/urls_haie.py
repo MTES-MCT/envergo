919e6639b22d429331548c61b770e41842b3a185
Thibault Jouannic <thibault@miximum.fr>
2024-08-01 11:39:00 +0200

    Plug the moulinette urls into the "haie" site

--- config/urls_haie.py
+++ config/urls_haie.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 
 from .urls import urlpatterns as common_urlpatterns
 
 urlpatterns = [
     path("", include("envergo.pages.urls_haie")),
+    path(_("moulinette/"), include("envergo.moulinette.urls")),
 ] + common_urlpatterns
