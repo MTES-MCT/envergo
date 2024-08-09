d37ab601ac3468704ca06974a969496c1cb1d185
PY <py.dezaunay@gmail.com>
2024-08-09 11:01:39 +0200

    remove useless things

--- envergo/moulinette/regulations/dep.py
+++ envergo/moulinette/regulations/dep.py
@@ -1,4 +1,3 @@
-from envergo.evaluations.models import RESULTS
 from envergo.moulinette.regulations import CriterionEvaluator
 
 
@@ -6,16 +5,8 @@ class DerogationEspecesProtegees(CriterionEvaluator):
     choice_label = "DEP > Dérogation espèces protégées"
     slug = "dep"
 
-    CODES = [
-        "soumis",
-    ]
-
     CODE_MATRIX = {
         "soumis": "soumis",
-    }
-
-    RESULT_MATRIX = {
-        "soumis": RESULTS.soumis,
     }
 
     def get_result_data(self):
