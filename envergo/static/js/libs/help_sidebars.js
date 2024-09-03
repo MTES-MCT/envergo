5804bfc48e50fff2c3a834d699f5e8026d992867
Thibault Jouannic <thibault@miximum.fr>
2024-09-03 16:00:36 +0200

    Rework the sidebar btn mechanism
    The same button will toggle the corresponding sidebar

--- envergo/static/js/libs/help_sidebars.js
+++ envergo/static/js/libs/help_sidebars.js
@@ -1,47 +1,76 @@
-/**
- * Handle the lateral help sidebar display and hiding.
- */
 window.addEventListener("load", function () {
-  let helpBtns = document.querySelectorAll(".help-sidebar-button");
-  let closeBtns = document.querySelectorAll(".fr-btn--close");
-  let moulinetteGrid = document.getElementById("moulinette-grid");
-  let sidebars = document.querySelectorAll(".help-sidebar");
 
-  // When a help button is clicked…
-  helpBtns.forEach(function (btn) {
-    btn.addEventListener("click", function () {
+  /**
+   * Handle the lateral help sidebar display and hiding.
+   *
+   * When a sidebar button is clicked, the corresponding sidebar is shown.
+   * when the sidebar is already open, it is hidden instead.
+   */
+  class SidebarManager {
+    constructor(sidebarElts, sidebarBtns, mainContent) {
+      this.sidebarElts = sidebarElts;
+      this.sidebarBtns = sidebarBtns;
+      this.mainContent = mainContent;
 
-      // Close all currently open sidebars
-      sidebars.forEach(function (sidebar) {
-        sidebar.close();
+      // Keep track of the currently open sidebar
+      this.currentSidebarId = undefined;
+
+      this.sidebarBtns.forEach((sidebarBtn) => {
+        sidebarBtn.addEventListener("click", this.onSidebarBtnClick.bind(this));
       });
 
-      // Display the sidebar that corresponds to the current field
-      let sidebarId = btn.attributes["aria-controls"].value;
-      let sidebar = document.getElementById(sidebarId);
-      sidebar.classList.add("sidebar-open");
-      sidebar.show();
+      this.sidebarElts.forEach((sidebarElt) => {
+        let closeBtn = sidebarElt.querySelector(".fr-btn--close");
+        closeBtn.addEventListener("click", this.closeSidebar.bind(this));
+      });
+    }
 
-      // Also add a class to the main moulinette content so we can adapt
-      // it's display
-      moulinetteGrid.classList.add("sidebar-open");
+    onSidebarBtnClick(evt) {
+      let btn = evt.currentTarget;
+      let sidebarId = btn.attributes["aria-controls"].value;
+
+      if (this.currentSidebarId === sidebarId) {
+        this.closeSidebar();
+      } else {
+        this.closeSidebar();
+        this.openSidebar(sidebarId);
+      }
+    }
+
+    // Open the selected sidebar
+    openSidebar(sidebarId) {
+      let sidebarElt = document.getElementById(sidebarId);
+      sidebarElt.show();
+      this.mainContent.classList.add("sidebar-open");
+
+      this.currentSidebarId = sidebarId;
 
       // Track matomo event
       let eventId = sidebarId.replace("sidebar-", "");
       _paq.push(["trackEvent", "Form", "HelpDisclose", eventId]);
-    });
+    }
 
-    closeBtns.forEach(function (btn) {
-      btn.addEventListener("click", function () {
-        let sidebarId = btn.attributes["aria-controls"].value;
-        let sidebar = document.getElementById(sidebarId);
-        sidebar.close();
-        moulinetteGrid.classList.remove("sidebar-open");
+    // Close the currently open sidebar (if any)
+    closeSidebar() {
 
-        // Track matomo event
-        let eventId = sidebarId.replace("sidebar-", "");
-        _paq.push(["trackEvent", "Form", "HelpConceal", eventId]);
-      });
-    });
-  });
+      if (this.currentSidebarId === undefined) {
+        return;
+      }
+
+      let sidebarElt = document.getElementById(this.currentSidebarId);
+      sidebarElt.close();
+      this.mainContent.classList.remove("sidebar-open");
+
+      // Track matomo event
+      let eventId = this.currentSidebarId.replace("sidebar-", "");
+      _paq.push(["trackEvent", "Form", "HelpConceal", eventId]);
+
+      this.currentSidebarId = undefined;
+    }
+  }
+
+  let sidebarElts = document.querySelectorAll(".help-sidebar");
+  let sidebarBtns = document.querySelectorAll(".help-sidebar-button");
+  let moulinetteGrid = document.getElementById("moulinette-grid");
+  new SidebarManager(sidebarElts, sidebarBtns, moulinetteGrid);
 });
