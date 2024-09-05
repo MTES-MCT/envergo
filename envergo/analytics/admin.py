84dd0ab45ba292992b684dcf5a5d7656f7f857ec
PY <py.dezaunay@gmail.com>
2024-09-05 06:51:54 +0200

    migration

--- envergo/analytics/admin.py
+++ envergo/analytics/admin.py
@@ -5,5 +5,5 @@ from envergo.analytics.models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ["category", "event", "session_key", "date_created"]
+    list_display = ["category", "event", "session_key", "date_created", "site"]
     search_fields = ["category", "event", "session_key"]
