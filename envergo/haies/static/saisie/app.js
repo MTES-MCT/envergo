64581b069e5894841ad48755b74b575bbbfcdac9
Thibault Jouannic <thibault@miximum.fr>
2024-09-06 10:25:00 +0200

    Display polylines length in realtime

--- envergo/haies/static/saisie/app.js
+++ envergo/haies/static/saisie/app.js
@@ -9,6 +9,14 @@ createApp({
   setup() {
     const polylines = ref([]);
 
+    const calculatePolylineLength = (latLngs) => {
+      let length = 0;
+      for (let i = 0; i < latLngs.length - 1; i++) {
+        length += latLngs[i].distanceTo(latLngs[i + 1]);
+      }
+      return length;
+    };
+
     // Initialiser la carte Leaflet après le montage du composant
     onMounted(() => {
       const map = L.map('map').setView([43.6861, 3.5911], 17);
@@ -25,19 +33,20 @@ createApp({
       map.on('click', (e) => {
         if (!currentPolyline) {
           currentPolyline = L.polyline([], { color: 'red' }).addTo(map);
+          polylines.value.push({ latLngs: currentPolyline.getLatLngs(), length: 0 });
         }
         currentPolyline.addLatLng(e.latlng);
+
+        const index = polylines.value.length - 1;
+        polylines.value[index].latLngs = currentPolyline.getLatLngs();
+        polylines.value[index].length = calculatePolylineLength(currentPolyline.getLatLngs());
       });
 
       // Terminer la polyline au double-clic
       map.on('dblclick', (evt) => {
         if (currentPolyline) {
-          polylines.value.push(currentPolyline.getLatLngs());
           currentPolyline = null;
         }
-
-        debugger;
-        evt.preventDefault();
       });
     });
 
