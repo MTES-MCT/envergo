7501cf37faa10be0e1a16a3b1a8884a5f73f804a
PY <py.dezaunay@gmail.com>
2024-09-13 12:00:34 +0200

    config

--- config/settings/ci.py
+++ config/settings/ci.py
@@ -3,11 +3,17 @@ With these settings, tests run faster.
 """
 
 from .base import *  # noqa
+from .base import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="xa7B39ApH5wH2VZS28Hk1CfLs7Hta6iPAkModtOEft8iGv3jppqDeGSCvpW1AX4p",
+)
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
