f0da54dce3a950c9a94a33e4844c49161b5c1227
Thibault Jouannic <thibault@miximum.fr>
2024-09-25 16:02:42 +0200

    Save data (wip)

--- envergo/haies/urls.py
+++ envergo/haies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from envergo.haies.views import Saisie
+from envergo.haies.views import Saisie, SaveHedgeDataView
 
 urlpatterns = [
     path("saisie/", Saisie.as_view(), name="saisie_haie"),
+    path("persist/", SaveHedgeDataView.as_view(), name="persist_hedges"),
 ]
