28c5c46fe65e8907ea1cd75a245c24a8373ae349
Thibault Jouannic <thibault@miximum.fr>
2024-10-02 10:24:11 +0200

    Display hedges input data in admin module

--- envergo/hedges/models.py
+++ envergo/hedges/models.py
@@ -1,6 +1,38 @@
 import uuid
 
+from django.contrib.gis.geos import LineString
 from django.db import models
+
+TO_PLANT = "TO_PLANT"
+TO_REMOVE = "TO_REMOVE"
+
+
+# WGS84, geodetic coordinates, units in degrees
+# Good for storing data and working wordwide
+EPSG_WGS84 = 4326
+
+# Projected coordinates
+# Used for displaying tiles in web map systems (OSM, GoogleMaps)
+# Good for working in meters
+EPSG_MERCATOR = 3857
+
+
+class Hedge:
+    """Represent a single hedge."""
+
+    def __init__(self, id, latLngs, type):
+        self.id = id
+        self.geometry = LineString(
+            [(latLng["lat"], latLng["lng"]) for latLng in latLngs], srid=EPSG_WGS84
+        )
+        self.geometry.transform(EPSG_MERCATOR)
+        self.type = type
+
+    @property
+    def length(self):
+        """TODO this gives a different value than leaflet. Need to investigate more."""
+
+        return int(self.geometry.length)
 
 
 class HedgeData(models.Model):
@@ -11,3 +43,12 @@ class HedgeData(models.Model):
     class Meta:
         verbose_name = "Hedge data"
         verbose_name_plural = "Hedge data"
+
+    def hedges(self):
+        return [Hedge(**h) for h in self.data]
+
+    def hedges_to_plant(self):
+        return [Hedge(**h) for h in self.data if h["type"] == TO_PLANT]
+
+    def hedges_to_remove(self):
+        return [Hedge(**h) for h in self.data if h["type"] == TO_REMOVE]
