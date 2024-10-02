6c839e28f075d7ac83a98ce55ef48a3bda18a4ba
Thibault Jouannic <thibault@miximum.fr>
2024-10-02 10:44:36 +0200

    Pass hedge input data back to main form

--- envergo/hedges/views.py
+++ envergo/hedges/views.py
@@ -20,7 +20,16 @@ class SaveHedgeDataView(CreateView):
         try:
             data = json.loads(request.body)
             hedge_data = HedgeData.objects.create(data=data)
-            return JsonResponse({"id": str(hedge_data.id)}, status=201)
+            response_data = {
+                "input_id": str(hedge_data.id),
+                "hedges_to_plant": len(hedge_data.hedges_to_plant()),
+                "length_to_plant": sum(h.length for h in hedge_data.hedges_to_plant()),
+                "hedges_to_remove": len(hedge_data.hedges_to_remove()),
+                "length_to_remove": sum(
+                    h.length for h in hedge_data.hedges_to_remove()
+                ),
+            }
+            return JsonResponse(response_data, status=201)
         except json.JSONDecodeError:
             return JsonResponse({"error": "Invalid JSON data"}, status=400)
         except Exception as e:
