01c04a808725613d495b7c13d23acec1e6718716
Thibault Jouannic <thibault@miximum.fr>
2024-10-04 11:08:47 +0200

    Add missing properties to the class

--- envergo/hedges/models.py
+++ envergo/hedges/models.py
@@ -21,7 +21,7 @@ class Hedge:
     """Represent a single hedge."""
 
     def __init__(self, id, latLngs, type):
-        self.id = id
+        self.id = id  # The edge reference, e.g A1, A2…
         self.geometry = LineString(
             [(latLng["lat"], latLng["lng"]) for latLng in latLngs], srid=EPSG_WGS84
         )
@@ -50,5 +50,11 @@ class HedgeData(models.Model):
     def hedges_to_plant(self):
         return [Hedge(**h) for h in self.data if h["type"] == TO_PLANT]
 
+    def length_to_plant(self):
+        return sum(h.length for h in self.hedges_to_plant())
+
     def hedges_to_remove(self):
         return [Hedge(**h) for h in self.data if h["type"] == TO_REMOVE]
+
+    def length_to_remove(self):
+        return sum(h.length for h in self.hedges_to_remove())
