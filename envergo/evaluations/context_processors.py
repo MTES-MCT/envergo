aa8cf0064867735765cca40659af5ed90ff3192f
PY <py.dezaunay@gmail.com>
2024-10-22 06:24:58 +0200

    tests fix

--- envergo/evaluations/context_processors.py
+++ envergo/evaluations/context_processors.py
@@ -3,6 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 def request_eval_context(request):
     context = {}
-    if request.path.startswith(f"/avis/{_("form/")}"):
+    if request.path.startswith(f"/avis/{_('form/')}"):
         context["is_request_btn_disabled"] = True
     return context
