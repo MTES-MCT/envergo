f0a6c568b1bd0d433751714b3a600c5adae34644
PY <py.dezaunay@gmail.com>
2024-12-19 11:40:06 +0100

    setup scalingo

--- bin/start_publicodes.sh
+++ bin/start_publicodes.sh
@@ -5,5 +5,11 @@ echo "Starting the Publicodes node app ($DJANGO_SETTINGS_MODULE) as user `whoami
 # Navigate to the publicodes directory
 cd publicodes
 
+# Install the Node.js dependencies
+npm install
+
+# add node_modules/.bin to the PATH
+export PATH=$PATH:./node_modules/.bin
+
 # Start the Node.js application
 npm run serve
