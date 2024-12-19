2e4a7cbfa2f0b9c23b63551c709b71f6c7e66513
PY <py.dezaunay@gmail.com>
2024-12-19 10:26:56 +0100

    scalingo setup

--- bin/start_publicodes.sh
+++ bin/start_publicodes.sh
@@ -2,5 +2,8 @@
 # This script is ran by scalingo to start the application
 
 echo "Starting the Publicodes node app ($DJANGO_SETTINGS_MODULE) as user `whoami`"
+# Navigate to the publicodes directory
+cd publicodes
 
+# Start the Node.js application
 npm run start
