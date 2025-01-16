84030422a125c82b639839c84a25df2dc440517a
Thibault Jouannic <thibault@miximum.fr>
2025-01-16 16:04:46 +0100

    Fix duplicate fields in list_display

--- envergo/users/admin.py
+++ envergo/users/admin.py
@@ -34,9 +34,6 @@ class UserAdmin(auth_admin.UserAdmin):
             {
                 "fields": (
                     "is_active",
-                    "is_confirmed_by_admin",
-                    "access_amenagement",
-                    "access_haie",
                     "is_staff",
                     "is_superuser",
                     "groups",
