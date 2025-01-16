9f26b1d215465d4bbaf52a856d519257d357f7ea
PY <py.dezaunay@gmail.com>
2025-01-16 09:10:09 +0100

    improve UI

--- envergo/petitions/urls_haie.py
+++ envergo/petitions/urls_haie.py
@@ -10,13 +10,13 @@ from envergo.petitions.views import (
 urlpatterns = [
     path("", PetitionProjectCreate.as_view(), name="petition_project_create"),
     path(
-        "<slug:reference>",
+        "<slug:reference>/",
         PetitionProjectDetail.as_view(),
         name="petition_project",
     ),
     # a path that redirects to the petition project detail page without logging the event
     path(
-        "<slug:reference>/auto-redirection",
+        "<slug:reference>/auto-redirection/",
         PetitionProjectAutoRedirection.as_view(),
         name="petition_project_auto_redirection",
     ),
