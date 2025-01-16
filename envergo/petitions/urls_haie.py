b13cdea685707ce66b425e700d6e920fa4efaf0c
PY <py.dezaunay@gmail.com>
2025-01-16 06:30:46 +0100

    graphical review + edit link + url

--- envergo/petitions/urls_haie.py
+++ envergo/petitions/urls_haie.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from envergo.petitions.views import (
     PetitionProjectAutoRedirection,
@@ -13,9 +14,15 @@ urlpatterns = [
         PetitionProjectDetail.as_view(),
         name="petition_project",
     ),
+    # This is another "fake" url, only for matomo tracking
+    path(
+        "+ref_proj+/consultation/haies/",
+        RedirectView.as_view(pattern_name="home"),
+        name="petition_project_hedges",
+    ),
     # a path that redirects to the petition project detail page without logging the event
     path(
-        "<slug:reference>/auto-redirection",
+        "<slug:reference>/auto-redirection/",
         PetitionProjectAutoRedirection.as_view(),
         name="petition_project_auto_redirection",
     ),
