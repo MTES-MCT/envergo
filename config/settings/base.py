d58c3320f388bab5c10b75039c1bcf6dc910ffeb
PY <py.dezaunay@gmail.com>
2025-02-10 15:00:13 +0100

    middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -142,6 +142,7 @@ MIDDLEWARE = [
     # "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "envergo.contrib.middleware.SetUrlConfBasedOnSite",
+    "envergo.contrib.middleware.MemoryUsageMiddleware",
     "envergo.analytics.middleware.SetVisitorIdCookie",
     "envergo.analytics.middleware.StoreMtmValues",
 ]
--- envergo/contrib/middleware.py
+++ envergo/contrib/middleware.py
@@ -1,5 +1,12 @@
+import io
+import logging
+import sys
+
+import objgraph
 from django.conf import settings
 from django.contrib.sites.models import Site
+
+logger = logging.getLogger(__name__)
 
 
 class SetUrlConfBasedOnSite:
@@ -25,3 +32,39 @@ class SetUrlConfBasedOnSite:
         response = self.get_response(request)
 
         return response
+
+
+class MemoryUsageMiddleware:
+    """Track memory usage of each request."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path.endswith(".js"):
+            return self.get_response(request)
+
+        leaking_objects = objgraph.get_leaking_objects()
+        leaking_object_size = sum(sys.getsizeof(obj) for obj in leaking_objects)
+        leaking_object_before_request_size_kib = leaking_object_size / 1024
+        # self.log_memory_usage(request, "Before request")  # Log object growth before request
+
+        response = self.get_response(request)
+
+        leaking_objects = objgraph.get_leaking_objects()
+        leaking_object_size = sum(sys.getsizeof(obj) for obj in leaking_objects)
+        leaking_object_after_request_size_kib = leaking_object_size / 1024
+        self.log_memory_usage(
+            request,
+            f"After request\nSize of leaking objects: "
+            f"{leaking_object_after_request_size_kib - leaking_object_before_request_size_kib:.2f} KiB",
+        )
+        return response
+
+    def log_memory_usage(self, request, message=None):
+        buffer = io.StringIO()
+        objgraph.show_growth(limit=100, file=buffer)
+        route_info = f"Route: {request.path}\n" if request else ""
+        logger.info(
+            f"{f'{message}\n' if message else ''}{route_info}{buffer.getvalue()}"
+        )
