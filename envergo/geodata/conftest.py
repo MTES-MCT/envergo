4d3c3040f1da053907fd53a9807da4d0cc840f98
Thibault Jouannic <thibault@miximum.fr>
2025-03-24 10:02:40 +0100

    Remove duplicate var declaration

--- envergo/geodata/conftest.py
+++ envergo/geodata/conftest.py
@@ -1,46 +1,13 @@
 import pytest
 from django.contrib.gis.geos import MultiPolygon, Polygon
 
-from envergo.geodata.tests.factories import DepartmentFactory, MapFactory, ZoneFactory
-
-# This is a rough pentagon that I manually drew on geoportail and that contains
-# France's mainland.
-france_polygon = Polygon(
-    [
-        (2.239523057461999, 51.37848260569899),
-        (-5.437949095065911, 48.830042871275225),
-        (-2.020973593289057, 42.22052255703733),
-        (7.672371135600932, 42.3263119734425),
-        (9.759728555096416, 49.41947007260785),
-        (2.239523057461999, 51.37848260569899),
-    ]
-)
-
-# Very rough department outlines
-loire_atlantique_polygon = Polygon(
-    [
-        (-2.318813217788111, 47.11172939002415),
-        (-1.8093222509912361, 46.85878309487171),
-        (-1.0224264990381111, 47.06497777827326),
-        (-1.336910141616236, 47.267582403961455),
-        (-0.8782309423974862, 47.364409358656644),
-        (-1.272365463881861, 47.826525823757436),
-        (-2.679988754897486, 47.46013043348137),
-        (-2.550899399428736, 47.13508980827845),
-        (-2.215816391616236, 47.213505682461204),
-        (-2.318813217788111, 47.11172939002415),
-    ]
-)
-
-herault_polygon = Polygon(
-    [
-        (3.215640301549349, 43.22794194612112),
-        (2.5426723773152715, 43.395887014114464),
-        (3.2656957338328425, 43.913967044500254),
-        (3.841333590843401, 43.96202635483297),
-        (4.262633800688122, 43.58351502379401),
-        (3.215640301549349, 43.22794194612112),
-    ]
+from envergo.geodata.tests.factories import (
+    DepartmentFactory,
+    MapFactory,
+    ZoneFactory,
+    france_polygon,
+    herault_polygon,
+    loire_atlantique_polygon,
 )
 
 
