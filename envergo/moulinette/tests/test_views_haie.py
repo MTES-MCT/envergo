72b7431dacf8d064f45218d2224450ccf9729634
EH <emgh@webeniste.net>
2025-04-16 15:24:07 +0200

    Add test

--- envergo/moulinette/tests/test_views_haie.py
+++ envergo/moulinette/tests/test_views_haie.py
@@ -1,6 +1,9 @@
+from urllib.parse import urlencode
+
 import pytest
 from django.test import override_settings
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 from envergo.moulinette.tests.factories import ConfigHaieFactory
 
@@ -36,3 +39,28 @@ def test_triage_result(client):
     content = res.content.decode()
     assert "Votre projet n'est pas encore pris en compte par le simulateur" in content
     assert "<h2>kikoo</h2>" not in content
+
+
+@pytest.mark.urls("config.urls_haie")
+@override_settings(ENVERGO_HAIE_DOMAIN="testserver")
+def test_debug_result(client):
+
+    ConfigHaieFactory()
+
+    data = {
+        "profil": "autre",
+        "element": "haie",
+        "motif": "chemin_acces",
+        "reimplantation": "remplacement",
+        "department": "44",
+        "debug": "true",
+        "haies": "abdc-1234",
+    }
+
+    url = reverse("moulinette_result")
+    params = urlencode(data)
+    full_url = f"{url}?{params}"
+    res = client.get(full_url)
+
+    assert res.status_code == 200
+    assertTemplateUsed(res, "haie/moulinette/result_debug.html")
