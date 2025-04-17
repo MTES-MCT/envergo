f6730357536b90d28b5b7c04dc0dc7c830d7e370
PY <py.dezaunay@gmail.com>
2025-04-17 11:21:28 +0200

    add department in url

--- envergo/hedges/urls.py
+++ envergo/hedges/urls.py
@@ -4,6 +4,10 @@ from envergo.hedges.views import HedgeInput, HedgeQualityView
 
 urlpatterns = [
     path("qualite/", HedgeQualityView.as_view(), name="hedge_quality"),
-    path("<str:mode>/", HedgeInput.as_view(), name="input_hedges"),
-    path("<str:mode>/<uuid:id>/", HedgeInput.as_view(), name="input_hedges"),
+    path("<str:department>/<str:mode>/", HedgeInput.as_view(), name="input_hedges"),
+    path(
+        "<str:department>/<str:mode>/<uuid:id>/",
+        HedgeInput.as_view(),
+        name="input_hedges",
+    ),
 ]
