5848528daf38f7c411282a480e066f29610c5138
Thibault Jouannic <thibault@miximum.fr>
2025-04-17 14:36:10 +0200

    Manage file import errors

--- envergo/hedges/tasks.py
+++ envergo/hedges/tasks.py
@@ -16,11 +16,16 @@ from envergo.hedges.models import (
 logger = logging.getLogger(__name__)
 
 
+ALL_HEDGE_TYPES = dict(HEDGE_TYPES).keys()
+
+
 @app.task(bind=True)
-@transaction.atomic
 def process_species_map_file(task, object_id):
+    """Process a single SpeciesMapFile objects.
+
+    This scripts import the file data and associatets Species with Maps.
+    """
     smf = SpeciesMapFile.objects.get(pk=object_id)
-    all_hedge_types = dict(HEDGE_TYPES).keys()
     import_log = []
 
     # Store the task data in the model, so we can display progression
@@ -34,7 +39,7 @@ def process_species_map_file(task, object_id):
     SpeciesMap.objects.filter(species_map_file=smf).delete()
 
     # Process csv file
-    species_map_files = []
+    species_maps = []
     with open(smf.file.path, "r") as csvfile:
         nb_lines = 0
         reader = csv.DictReader(csvfile)
@@ -42,32 +47,22 @@ def process_species_map_file(task, object_id):
             nb_lines += 1
 
             try:
-                species = Species.objects.get(taxref_ids__contains=[row["CD_NOM"]])
+                species_map = process_species_file_map_row(row, smf)
+                species_maps.append(species_map)
             except Species.DoesNotExist:
-                msg = f"Espèce inconnue {row["Nom commun"]}"
+                if "Nom commun" in row:
+                    msg = f"Espèce inconnue {row["Nom commun"]}"
+                else:
+                    msg = f"Espèce inconnue à la ligne {nb_lines + 1}"
                 import_log.append(msg)
-                continue
-
-            hedge_types = []
-            for hedge_type in all_hedge_types:
-                if bool(row[hedge_type]):
-                    hedge_types.append(hedge_type)
-
-            species_map_files.append(
-                SpeciesMap(
-                    species=species,
-                    map=smf.map,
-                    species_map_file=smf,
-                    hedge_types=hedge_types,
-                    proximite_mare=bool(row["proximite_mare"]),
-                    proximite_point_eau=bool(row["proximite_point_eau"]),
-                    connexion_boisement=bool(row["connexion_boisement"]),
-                    vieil_arbre=bool(row["vieil_arbre"]),
-                )
-            )
+                logger.warning(msg)
+            except Exception as e:
+                msg = f"Erreur d'import sur la ligne {nb_lines + 1}: {e}"
+                import_log.append(msg)
+                logger.error(msg)
 
     # Create objects with a single query
-    objects = SpeciesMap.objects.bulk_create(species_map_files)
+    objects = SpeciesMap.objects.bulk_create(species_maps)
 
     # Update the import status and metadata
     if len(objects) == nb_lines:
@@ -81,3 +76,25 @@ def process_species_map_file(task, object_id):
     smf.import_date = timezone.now()
     smf.import_log = "\n".join(import_log)
     smf.save()
+
+
+def process_species_file_map_row(row, smf):
+    """Process a single file row."""
+    species_taxref_id = row["CD_NOM"]
+    species = Species.objects.get(taxref_ids__contains=[species_taxref_id])
+
+    hedge_types = []
+    for hedge_type in ALL_HEDGE_TYPES:
+        if bool(row[hedge_type]):
+            hedge_types.append(hedge_type)
+
+    return SpeciesMap(
+        species=species,
+        map=smf.map,
+        species_map_file=smf,
+        hedge_types=hedge_types,
+        proximite_mare=bool(row["proximite_mare"]),
+        proximite_point_eau=bool(row["proximite_point_eau"]),
+        connexion_boisement=bool(row["connexion_boisement"]),
+        vieil_arbre=bool(row["vieil_arbre"]),
+    )
