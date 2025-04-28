aba062d6fa54642d6191ff49823aa248862e6a97
EH <emgh@webeniste.net>
2025-04-28 17:11:16 +0200

    Use department foreignkey

--- envergo/petitions/tests/test_models.py
+++ envergo/petitions/tests/test_models.py
@@ -9,4 +9,4 @@ pytestmark = pytest.mark.django_db
 def test_set_department_on_save():
     ConfigHaieFactory()
     petition_project = PetitionProjectFactory()
-    assert petition_project.department_code == "44"
+    assert petition_project.department.department == "44"
