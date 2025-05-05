320621a2619c9d2376aabefd1e60e18400a36f4a
PY <py.dezaunay@gmail.com>
2025-05-05 10:42:57 +0200

    add evaluator

--- envergo/moulinette/regulations/ep.py
+++ envergo/moulinette/regulations/ep.py
@@ -82,3 +82,84 @@ class EspecesProtegeesAisne(PlantationConditionMixin, EPMixin, CriterionEvaluato
 
     def get_replantation_coefficient(self):
         return D("1.5")
+
+
+class EspecesProtegeesNormandie(PlantationConditionMixin, EPMixin, CriterionEvaluator):
+    """Check for protected species living in hedges."""
+
+    choice_label = "EP > EP Normandie"
+    slug = "ep_normandie"
+    plantation_conditions = [SafetyCondition]
+
+    RESULT_MATRIX = {
+        "interdit": RESULTS.interdit,
+        "interdit_remplacement": RESULTS.interdit,
+        "derogation_simplifiee": RESULTS.derogation_simplifiee,
+        "dispense_coupe_a_blanc": RESULTS.dispense_sous_condition,
+        "dispense_20m": RESULTS.dispense_sous_condition,
+        "dispense_10m": RESULTS.dispense,
+    }
+
+    CODE_MATRIX = {
+        ("0", False, "non"): "dispense_10m",
+        ("0", False, "remplacement"): "dispense_10m",
+        ("0", False, "replantation"): "dispense_10m",
+        ("lte_1", False, "non"): "interdit",
+        ("lte_1", False, "remplacement"): "dispense_20m",
+        ("lte_1", False, "replantation"): "dispense_20m",
+        ("gt_1", False, "non"): "interdit",
+        ("gt_1", False, "remplacement"): "interdit_remplacement",
+        ("gt_1", False, "replantation"): "derogation_simplifiee",
+        ("0", True, "non"): "dispense_10m",
+        ("0", True, "remplacement"): "dispense_10m",
+        ("0", True, "replantation"): "dispense_10m",
+        ("lte_1", True, "non"): "interdit",
+        ("lte_1", True, "remplacement"): "dispense_coupe_a_blanc",
+        ("lte_1", True, "replantation"): "dispense_20m",
+        ("gt_1", True, "non"): "interdit",
+        ("gt_1", True, "remplacement"): "dispense_coupe_a_blanc",
+        ("gt_1", True, "replantation"): "derogation_simplifiee",
+    }
+
+    def get_result_data(self):
+        haies = self.catalog.get("haies")
+        all_r = []
+        coupe_a_blanc_every_hedge = True
+        reimplantation = self.catalog.get("reimplantation")
+        self._minimum_length_to_plant = 0.0
+
+        if haies:
+            for hedge in haies.hedges_to_remove():
+                if hedge.mode_destruction != "coupe_a_blanc":
+                    coupe_a_blanc_every_hedge = False
+
+                if hedge.length <= 10:
+                    r = 0
+                elif hedge.length <= 20:
+                    r = 1
+                elif (
+                    reimplantation == "remplacement"
+                    and hedge.mode_destruction == "coupe_a_blanc"
+                ):
+                    r = 1
+                else:
+                    r = self.get_replantation_coefficient()
+                all_r.append(r)
+                self._minimum_length_to_plant = (
+                    self._minimum_length_to_plant + hedge.length * r
+                )
+
+        r_max = max(all_r) if all_r else self.get_replantation_coefficient()
+        r_max_value = "0" if r_max == 0 else "lte_1" if r_max <= 1 else "gt_1"
+
+        return r_max_value, coupe_a_blanc_every_hedge, reimplantation
+
+    def get_replantation_coefficient(self):
+        return 2
+
+    @property
+    def minimum_length_to_plant(self):
+        if not hasattr(self, "_result"):
+            raise RuntimeError("Call the evaluator `evaluate` method first")
+
+        return self._minimum_length_to_plant
