cf9aaeef2a7540ccd3ec6689212b518f012b8a88
PY <py.dezaunay@gmail.com>
2025-05-22 10:42:41 +0200

    handle default value for hedge cases

--- envergo/moulinette/regulations/ep.py
+++ envergo/moulinette/regulations/ep.py
@@ -253,11 +253,35 @@ class EspecesProtegeesNormandie(
         ("arbustive", "lt_0.5", "groupe_normandie_5"): D("1.8"),
         ("alignement", "lt_0.5", "groupe_normandie_5"): D("1.8"),
         ("mixte", "lt_0.5", "groupe_normandie_5"): D("2"),
+        ("degradee", "gt_1.6", "groupe_normandie_absent"): D("1"),
+        ("buissonnante", "gt_1.6", "groupe_normandie_absent"): D("1"),
+        ("arbustive", "gt_1.6", "groupe_normandie_absent"): D("1"),
+        ("alignement", "gt_1.6", "groupe_normandie_absent"): D("1"),
+        ("mixte", "gt_1.6", "groupe_normandie_absent"): D("1.2"),
+        ("degradee", "gt_1.2_lte_1.6", "groupe_normandie_absent"): D("1"),
+        ("buissonnante", "gt_1.2_lte_1.6", "groupe_normandie_absent"): D("1"),
+        ("arbustive", "gt_1.2_lte_1.6", "groupe_normandie_absent"): D("1.2"),
+        ("alignement", "gt_1.2_lte_1.6", "groupe_normandie_absent"): D("1.2"),
+        ("mixte", "gt_1.2_lte_1.6", "groupe_normandie_absent"): D("1.4"),
+        ("degradee", "gte_0.8_lte_1.2", "groupe_normandie_absent"): D("1"),
+        ("buissonnante", "gte_0.8_lte_1.2", "groupe_normandie_absent"): D("1.2"),
+        ("arbustive", "gte_0.8_lte_1.2", "groupe_normandie_absent"): D("1.4"),
+        ("alignement", "gte_0.8_lte_1.2", "groupe_normandie_absent"): D("1.4"),
+        ("mixte", "gte_0.8_lte_1.2", "groupe_normandie_absent"): D("1.6"),
+        ("degradee", "gte_0.5_lt_0.8", "groupe_normandie_absent"): D("1.4"),
+        ("buissonnante", "gte_0.5_lt_0.8", "groupe_normandie_absent"): D("1.6"),
+        ("arbustive", "gte_0.5_lt_0.8", "groupe_normandie_absent"): D("1.8"),
+        ("alignement", "gte_0.5_lt_0.8", "groupe_normandie_absent"): D("1.8"),
+        ("mixte", "gte_0.5_lt_0.8", "groupe_normandie_absent"): D("2.2"),
+        ("degradee", "lt_0.5", "groupe_normandie_absent"): D("1.8"),
+        ("buissonnante", "lt_0.5", "groupe_normandie_absent"): D("2"),
+        ("arbustive", "lt_0.5", "groupe_normandie_absent"): D("2.2"),
+        ("alignement", "lt_0.5", "groupe_normandie_absent"): D("2.2"),
+        ("mixte", "lt_0.5", "groupe_normandie_absent"): D("2.6"),
     }
 
     def get_catalog_data(self):
         catalog = super().get_catalog_data()
-        catalog["is_available"] = True
         haies = self.catalog.get("haies")
         all_r = []
         coupe_a_blanc_every_hedge = True
@@ -277,9 +301,16 @@ class EspecesProtegeesNormandie(
             map__map_type=MAP_TYPES.zonage,
         ).first()
 
-        zone_id = zonage.attributes.get("indentifiant_zone", None) if zonage else None
+        zone_id = (
+            zonage.attributes.get("indentifiant_zone", "groupe_normandie_absent")
+            if zonage
+            else "groupe_normandie_absent"
+        )
 
-        density_ratio = density_200 / density_5000 if density_5000 != 0 else 1000
+        # If the density at 5km is 0, this means that we're in a hedge case (desert, sea, other?)
+        # We then pick a coefficient corresponding to the Normandie average : 1
+        density_ratio = density_200 / density_5000 if density_5000 != 0 else 1
+
         if density_ratio > 1.6:
             density_ratio_range = "gt_1.6"
         elif density_ratio > 1.2:
@@ -306,11 +337,6 @@ class EspecesProtegeesNormandie(
                 ):
                     r = D(1)
                 else:
-                    if zone_id is None:
-                        # the centroid of the hedges to remove is not in an identified zone, and we need it to define R
-                        # The evaluator should return a "non disponible" result.
-                        catalog["is_available"] = False
-
                     r = self.COEFFICIENT_MATRIX.get(
                         (hedge.hedge_type, density_ratio_range, zone_id)
                     )
@@ -328,6 +354,8 @@ class EspecesProtegeesNormandie(
         catalog["r_max"] = r_max
         catalog["coupe_a_blanc_every_hedge"] = coupe_a_blanc_every_hedge
         catalog["aggregated_r"] = aggregated_r
+        catalog["density_ratio"] = density_ratio
+        catalog["density_zone"] = zone_id
         return catalog
 
     def get_result_data(self):
@@ -337,14 +365,6 @@ class EspecesProtegeesNormandie(
         r_max_value = "0" if r_max == 0 else "lte_1" if r_max <= 1 else "gt_1"
 
         return r_max_value, coupe_a_blanc_every_hedge, reimplantation
-
-    def get_result_code(self, result_data):
-        """Compute a unique result code from criterion data."""
-        is_available = self.catalog.get("is_available")
-        if not is_available:
-            return RESULTS.non_disponible
-
-        return super().get_result_code(result_data)
 
     def get_replantation_coefficient(self):
         return self.catalog.get("aggregated_r")
