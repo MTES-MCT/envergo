b43c4db34a27a7027687256a2b1c07086d6c86c4
EH <emgh@webeniste.net>
2025-06-16 16:29:33 +0200

    Update success url for onagre and notes

--- envergo/petitions/views.py
+++ envergo/petitions/views.py
@@ -637,6 +637,11 @@ class PetitionProjectInstructorRegulationView(PetitionProjectInstructorView):
             context["current_regulation"] = current_regulation
         return context
 
+    def get_success_url(self):
+        return reverse(
+            "petition_project_instructor_regulation_view", kwargs=self.kwargs
+        )
+
 
 class PetitionProjectInstructorDossierDSView(
     PetitionProjectInstructorMixin, DetailView
@@ -680,6 +685,9 @@ class PetitionProjectInstructorNotesView(PetitionProjectInstructorView):
 
     template_name = "haie/petitions/instructor_view_notes.html"
     matomo_tag = ""
+
+    def get_success_url(self):
+        return reverse("petition_project_instructor_notes_view", kwargs=self.kwargs)
 
 
 class PetitionProjectHedgeDataExport(DetailView):
