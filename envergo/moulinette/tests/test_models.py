3c44ef48b55d643fd38f75760944eb916545da2c
Thibault Jouannic <thibault@miximum.fr>
2025-06-23 09:55:13 +0200

    Fix broken tests

--- envergo/moulinette/tests/test_models.py
+++ envergo/moulinette/tests/test_models.py
@@ -225,11 +225,11 @@ def test_regulation_with_map_factory_can_create_a_location_centric_map(
         get_map_center=MagicMock(return_value=(47.0, -1.0))
     )
     PerimeterFactory(
-        regulation=regulation,
+        regulations=[regulation],
         activation_map=france_map,
     )
     PerimeterFactory(
-        regulation=regulation,
+        regulations=[regulation],
         activation_map=france_map,
     )
     assert regulation.map
@@ -264,11 +264,11 @@ def test_regulation_with_map_factory_can_create_a_hedge_to_remove_map(
         },
     )
     PerimeterFactory(
-        regulation=regulation,
+        regulations=[regulation],
         activation_map=france_map,
     )
     PerimeterFactory(
-        regulation=regulation,
+        regulations=[regulation],
         activation_map=france_map,
     )
     assert regulation.map
