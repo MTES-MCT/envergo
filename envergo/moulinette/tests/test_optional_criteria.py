41963ce2a2193ff2f5444469204b9c7a46e04182
Thibault Jouannic <thibault@miximum.fr>
2025-06-26 17:14:52 +0200

    Fix a broken test

--- envergo/moulinette/tests/test_optional_criteria.py
+++ envergo/moulinette/tests/test_optional_criteria.py
@@ -128,5 +128,5 @@ def test_optional_criterion_activation(admin_client):
     full_url = f"{url}?{params}"
     res = admin_client.get(full_url)
 
-    assert res.status_code == 200
-    assertTemplateUsed(res, "moulinette/home.html")
+    assert res.status_code == 302
+    assert res["Location"].startswith("/simulateur/formulaire/")
