884dade3dd8c704dfe01f46a8c80d84cbd9c9df7
Thibault Jouannic <thibault@miximum.fr>
2025-06-26 15:52:47 +0200

    Remove useless references to edit parameter

--- envergo/moulinette/views.py
+++ envergo/moulinette/views.py
@@ -27,7 +27,7 @@ from envergo.moulinette.models import (
     get_moulinette_class_from_site,
 )
 from envergo.moulinette.utils import compute_surfaces
-from envergo.utils.urls import remove_from_qs, update_qs
+from envergo.utils.urls import copy_qs, remove_from_qs, update_qs
 
 
 class MoulinetteMixin:
@@ -346,7 +346,6 @@ class MoulinetteResultMixin:
         moulinette = self.moulinette
         triage_form = self.triage_form
         is_debug = bool(self.request.GET.get("debug", False))
-        is_edit = bool(self.request.GET.get("edit", False))
         is_admin = self.request.user.is_staff
 
         # We want to display the moulinette result template, but must check all
@@ -359,8 +358,6 @@ class MoulinetteResultMixin:
             template_name = MoulinetteHaie.get_triage_template(triage_form)
         elif is_debug:
             template_name = moulinette.get_debug_result_template()
-        elif is_edit:
-            template_name = moulinette.get_home_template()
         elif not moulinette.has_config():
             template_name = moulinette.get_result_non_disponible_template()
         elif not (moulinette.is_evaluation_available() or is_admin):
@@ -406,7 +403,6 @@ class MoulinetteResultMixin:
         data = {}
         moulinette = context.get("moulinette", None)
         is_debug = bool(self.request.GET.get("debug", False))
-        is_edit = bool(self.request.GET.get("edit", False))
         is_alternative = bool(self.request.GET.get("alternative", False))
         # Let's build custom uris for better matomo tracking
         # Depending on the moulinette result, we want to track different uris
@@ -424,8 +420,6 @@ class MoulinetteResultMixin:
         missing_data_url = self.request.build_absolute_uri(
             reverse("moulinette_missing_data")
         )
-        form_url = self.request.build_absolute_uri(reverse("moulinette_form"))
-        form_url_with_edit = update_qs(form_url, {"edit": "true"})
         out_of_scope_result_url = self.request.build_absolute_uri(
             reverse("moulinette_result_out_of_scope")
         )
@@ -437,12 +431,7 @@ class MoulinetteResultMixin:
             bare_url, self.request
         )
 
-        if moulinette and is_edit:
-            data["matomo_custom_url"] = update_url_with_matomo_params(
-                form_url_with_edit, self.request
-            )
-
-        elif moulinette and is_debug:
+        if moulinette and is_debug:
             data["matomo_custom_url"] = update_url_with_matomo_params(
                 debug_url, self.request
             )
@@ -484,8 +473,10 @@ class MoulinetteResultMixin:
         share_print_url = update_qs(current_url, {"mtm_campaign": "print-simu"})
         result_url = remove_from_qs(current_url, "debug")
         debug_result_url = update_qs(current_url, {"debug": "true"})
+        form_url = reverse("moulinette_form")
+        form_url = copy_qs(form_url, current_url)
         edit_url = (
-            update_qs(result_url, {"edit": "true"})
+            update_qs(form_url, {"edit": "true"})
             if moulinette
             else context.get("triage_url", None)
         )
@@ -534,7 +525,6 @@ class MoulinetteResultMixin:
 
 class BaseMoulinetteResult(FormView):
     def get(self, request, *args, **kwargs):
-        is_edit = bool(self.request.GET.get("edit", False))
         context = self.get_context_data(**kwargs)
         res = self.render_to_response(context)
         moulinette = self.moulinette
@@ -544,21 +534,15 @@ class BaseMoulinetteResult(FormView):
             return HttpResponseRedirect(context["redirect_url"])
 
         elif moulinette:
-            if (
-                "debug" not in self.request.GET
-                and not is_edit
-                and not self.validate_results_url(request, context)
+            if "debug" not in self.request.GET and not self.validate_results_url(
+                request, context
             ):
                 return HttpResponseRedirect(self.get_results_url(context["form"]))
 
             required_form_errors = moulinette.required_form_errors()
             optional_form_errors = moulinette.optional_form_errors()
 
-            if not (
-                moulinette.has_missing_data()
-                or is_request_from_a_bot(request)
-                or is_edit
-            ):
+            if not (moulinette.has_missing_data() or is_request_from_a_bot(request)):
                 self.log_moulinette_event(moulinette, context)
             elif (
                 bool(required_form_errors)
@@ -663,16 +647,13 @@ class MoulinetteResultPlantation(MoulinetteHaieResult):
         """Check which template to use depending on the moulinette result."""
 
         moulinette = self.moulinette
-        is_edit = bool(self.request.GET.get("edit", False))
 
         # Moulinette result template for plantation is not the moulinette ABC class result template
         # So we get the template name super and check specific cases
 
         template_name = super().get_template_names()[0]
 
-        if is_edit:
-            template_name = "TODO"  # TODO
-        elif moulinette.has_missing_data():  # TODO missing only hedges to plant
+        if moulinette.has_missing_data():  # TODO missing only hedges to plant
             template_name = moulinette.get_result_template()
         elif template_name == "haie/moulinette/result.html":
             template_name = "haie/moulinette/result_plantation.html"
@@ -697,8 +678,8 @@ class MoulinetteResultPlantation(MoulinetteHaieResult):
             plantation_url = update_qs(plantation_url, self.request.GET)
             context["plantation_url"] = plantation_url
 
-        result_d_url = update_qs(reverse("moulinette_result"), self.request.GET)
-        context["edit_url"] = update_qs(result_d_url, {"edit": "true"})
+        form_url = update_qs(reverse("moulinette_form"), self.request.GET)
+        context["edit_url"] = update_qs(form_url, {"edit": "true"})
         return context
 
     def log_moulinette_event(self, moulinette, context, **kwargs):
@@ -755,7 +736,7 @@ class Triage(FormView):
         if (
             query_params["element"] == "haie"
             and query_params["travaux"] == "destruction"
-        ) and "edit" not in self.request.GET:
+        ):
             url = reverse("moulinette_form")
         else:
             url = reverse("moulinette_result")
