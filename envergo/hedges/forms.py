2d849462e84404ee18283296737e15857d12bf5b
EH <emgh@webeniste.net>
2025-07-21 08:36:44 +0200

    Fix python error

--- envergo/hedges/forms.py
+++ envergo/hedges/forms.py
@@ -40,12 +40,16 @@ class HedgePropertiesBaseForm(forms.Form):
 MODE_DESTRUCTION_CHOICES = (
     ("arrachage", "Arrachage"),
     (
-        "coupe_a_blanc", 
+        "coupe_a_blanc",
         mark_safe(
-            "Coupe à blanc (sur essence ne recépant pas)"
-            '<span class="fr-hint-text"><a href="https://www.notion.so/Liste-des-essences-et-leur-capacit-rec-per-1b6fe5fe47668041a5d9d22ac5be31e1" target="_blank" rel="noopener">Liste des essences ne recépant pas</a></span>'
+            """Coupe à blanc (sur essence ne recépant pas)
+            <span class="fr-hint-text">
+            <a href="https://www.notion.so/Liste-des-essences-et-leur-capacit-rec-per-1b6fe5fe47668041a5d9d22ac5be31e1"
+               target="_blank" rel="noopener">
+            Liste des essences ne recépant pas</a></span>
+            """
         ),
-        "Coupe à blanc (sur essence ne recépant pas)"
+        "Coupe à blanc (sur essence ne recépant pas)",
     ),
     ("autre", "Autre"),
 )
@@ -131,7 +135,7 @@ class SurTalusMixin(forms.Form):
 
 
 class InterchampMixin(forms.Form):
-    interchamp = forms.BooleanField(
+    interchamp = DisplayBooleanField(
         label=mark_safe(
             'Haie inter-champ <span class="fr-hint-text">Y compris en bord de chemin entre deux parcelles</span>'
         ),
