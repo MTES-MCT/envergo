6967b91361de493de0028d801271218175558662
Thibault Jouannic <thibault@miximum.fr>
2025-08-04 14:46:40 +0200

    Make the e2e test pass

--- e2e/haie/moulinette/form.spec.ts
+++ e2e/haie/moulinette/form.spec.ts
@@ -17,14 +17,14 @@ test('A petitioner can submit a project', async ({ page }) => {
     await page.locator('#hedge-input-iframe').contentFrame().getByRole('button', { name: 'Tracer une haie à détruire' }).click();
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').click({ position: { x: 300, y: 215 } });
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').dblclick({ position: { x: 310, y: 215 } });
-    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie D1' }).getByLabel("Alignement d'abres").check();
+    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie D1' }).getByText("Alignement d'arbres").check();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie D1').getByText('Bord de route, voie ou chemin').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie D1').getByText('Située sur une parcelle PAC').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie D1').getByRole('button', { name: 'Enregistrer' }).click();
     await page.locator('#hedge-input-iframe').contentFrame().getByRole('button', { name: 'Tracer une haie à détruire' }).click();
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').click({ position: { x: 400, y: 215 } });
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').dblclick({ position: { x: 410, y: 215 } });
-    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie D2' }).getByLabel('Haie mixte').check();
+    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie D2' }).getByText('Haie mixte').check();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie D2').getByText('Mare à moins de 200 m').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie D2').getByText('Située sur une parcelle PAC').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie D2').getByRole('button', { name: 'Enregistrer' }).click();
@@ -49,7 +49,7 @@ test('A petitioner can submit a project', async ({ page }) => {
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').click({ position: { x: 440, y: 215 } });
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').click({ position: { x: 440, y: 275 } });
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').dblclick({ position: { x: 400, y: 275 } });
-    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie P1' }).getByLabel("Alignement d'arbres").check();
+    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie P1' }).getByText("Alignement d'arbres").check();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie P1').getByText('Bord de route, voie ou chemin').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie P1').getByText('Située sur une parcelle PAC').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie P1').getByRole('button', { name: 'Enregistrer' }).click();
@@ -64,7 +64,7 @@ test('A petitioner can submit a project', async ({ page }) => {
     await page.locator('#hedge-input-iframe').contentFrame().getByRole('button', { name: 'Tracer une haie à planter' }).click();
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').click({ position: { x: 400, y: 215 } });
     await page.locator('#hedge-input-iframe').contentFrame().locator('#map').dblclick({ position: { x: 440, y: 275 } });
-    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie P2' }).getByLabel('Haie mixte').check();
+    await page.locator('#hedge-input-iframe').contentFrame().getByRole('dialog', { name: 'Description de la haie P2' }).getByText('Haie mixte').check();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie P2').getByText('Située sur une parcelle PAC').click();
     await page.locator('#hedge-input-iframe').contentFrame().getByLabel('Description de la haie P2').getByRole('button', { name: 'Enregistrer' }).click();
     await expect(page.locator('#hedge-input-iframe').contentFrame().getByText('Plantation adéquate')).toBeVisible();
