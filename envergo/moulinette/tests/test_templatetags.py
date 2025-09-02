110637926e6e6d625da2e9783642730ea99dbf45
EH <emgh@webeniste.net>
2025-09-02 19:25:36 +0200

    Change template filter to tag

--- envergo/moulinette/tests/test_templatetags.py
+++ envergo/moulinette/tests/test_templatetags.py
@@ -1,6 +1,6 @@
-from envergo.moulinette.templatetags.moulinette import display_motif
+from envergo.moulinette.templatetags.moulinette import humanize_motif
 
 
 def test_display_choice():
-    result = display_motif("amelioration_culture")
+    result = humanize_motif("amelioration_culture")
     assert "Amélioration des conditions d’exploitation agricole" in result
