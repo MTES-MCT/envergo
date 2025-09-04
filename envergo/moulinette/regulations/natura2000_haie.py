c5975de22d7d760e39d62c95fedc5aaa959a74ba
Thibault Jouannic <thibault@miximum.fr>
2025-09-04 15:00:33 +0200

    Compute detail for hedges n2000 intersection

--- envergo/moulinette/regulations/natura2000_haie.py
+++ envergo/moulinette/regulations/natura2000_haie.py
@@ -1,10 +1,8 @@
 from django import forms
-from django.contrib.gis.geos import GEOSGeometry
-from django.db.models import Exists, OuterRef
-from shapely import MultiLineString
+from django.contrib.gis.db.models.functions import Intersection
+from django.contrib.gis.geos import MultiLineString
 
 from envergo.evaluations.models import RESULTS
-from envergo.geodata.models import Zone
 from envergo.moulinette.regulations import CriterionEvaluator
 
 
@@ -15,6 +13,10 @@ class Natura2000HaieSettings(forms.Form):
         required=True,
         choices=RESULTS,
     )
+
+
+EPSG_WGS84 = 4326
+EPSG_LAMB93 = 2154
 
 
 class Natura2000Haie(CriterionEvaluator):
@@ -28,6 +30,44 @@ class Natura2000Haie(CriterionEvaluator):
         "soumis": RESULTS.soumis,
     }
 
+    def get_catalog_data(self):
+        """Let's compute the length of hedges crossing the N2000 perimeter."""
+
+        hedges = self.catalog["haies"].hedges_to_remove()
+
+        all_hedges = MultiLineString([h.geos_geometry for h in hedges], srid=EPSG_WGS84)
+
+        hors_alignement = MultiLineString(
+            [h.geos_geometry for h in hedges if h.hedge_type != "alignement"],
+            srid=EPSG_WGS84,
+        )
+
+        alignement = MultiLineString(
+            [h.geos_geometry for h in hedges if h.hedge_type == "alignement"],
+            srid=EPSG_WGS84,
+        )
+
+        qs = (
+            self.moulinette.natura2000_haie.natura2000_haie.activation_map.zones.all()
+            .filter(geometry__intersects=all_hedges)
+            .annotate(hors_alignement=Intersection(hors_alignement, "geometry"))
+            .annotate(alignement=Intersection(alignement, "geometry"))
+        )
+
+        l_n2000_hors_aa = 0.0
+        l_n2000_aa = 0.0
+        for zone in qs:
+            l_n2000_hors_aa += zone.hors_alignement.transform(
+                EPSG_LAMB93, clone=True
+            ).length
+            l_n2000_aa += zone.alignement.transform(EPSG_LAMB93, clone=True).length
+
+        data = {}
+        data["l_n2000_hors_aa"] = l_n2000_hors_aa
+        data["l_n2000_aa"] = l_n2000_aa
+
+        return data
+
     def get_result_data(self):
         """Returns if a non-alignement hedge intersects the n2000 zone.
 
@@ -36,36 +76,7 @@ class Natura2000Haie(CriterionEvaluator):
         specific check again.
         """
 
-        # We have to import here to prevent circular import error
-        from envergo.moulinette.fields import classpath
-        from envergo.moulinette.models import Criterion
-
-        hedges_except_alignement = [
-            h
-            for h in self.catalog["haies"].hedges_to_remove()
-            if h.hedge_type != "alignement"
-        ]
-
-        if len(hedges_except_alignement) == 0:
-            # If we are here, it means all hedges are of type "alignement d'arbres"
-            # which are not concerned by this criterion
-            return False
-
-        # Some non alignement hedges remain, we need to check if they intersect the
-        # n2000 perimeter
-        lines = [h.geometry for h in hedges_except_alignement]
-        multiline = MultiLineString(lines)
-        geometry = GEOSGeometry(multiline.wkb_hex)
-
-        zone_subquery = (
-            Zone.objects.filter(map_id=OuterRef("activation_map_id"))
-            .filter(geometry__intersects=geometry)
-            .values("id")
-        )
-        qs = Criterion.objects.filter(evaluator=classpath(self.__class__)).filter(
-            Exists(zone_subquery)
-        )
-        return qs.exists()
+        return self.catalog["l_n2000_hors_aa"] > 0.0
 
     def get_result_code(self, intersects_n2000):
         if intersects_n2000:
