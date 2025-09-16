39aefd762fe13ae603ace3b5206d411b426b5fee
EH <emgh@webeniste.net>
2025-09-16 11:53:15 +0200

    Remove maps url from alignement arbre context

--- envergo/petitions/regulations/alignementarbres.py
+++ envergo/petitions/regulations/alignementarbres.py
@@ -1,4 +1,3 @@
-from envergo.geodata.utils import get_google_maps_centered_url, get_ign_centered_url
 from envergo.hedges.regulations import TreeAlignmentsCondition
 from envergo.moulinette.forms import MOTIF_CHOICES
 from envergo.moulinette.regulations.alignementarbres import AlignementsArbres
@@ -20,8 +19,6 @@ def alignement_arbres_get_instructor_view_context(
     context = {
         "motif": next((v[1] for v in MOTIF_CHOICES if v[0] == motif), motif),
         "replantation_coefficient": R,
-        "ign_url": get_ign_centered_url(petition_project.hedge_data),
-        "google_maps_url": get_google_maps_centered_url(petition_project.hedge_data),
     }
 
     # Hedges to remove, alignement_arbres, en bord de voie
