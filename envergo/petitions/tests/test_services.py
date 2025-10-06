fed6fd4238fe14708019e61f711a51113c2a9ad7
Thibault Jouannic <thibault@miximum.fr>
2025-10-06 10:13:57 +0200

    Fix bad test data

--- envergo/petitions/tests/test_services.py
+++ envergo/petitions/tests/test_services.py
@@ -388,6 +388,7 @@ def test_ep_aisne_get_instructor_view_context(france_map):  # noqa
         "travaux": "destruction",
         "element": "haie",
         "department": 44,
+        "numero_pacage": "123456789",
     }
     moulinette_data = {"initial": data, "data": data}
 
@@ -652,6 +653,7 @@ def test_bcae8_get_instructor_view_context(france_map):  # noqa
         "element": "haie",
         "department": 44,
         "lineaire_total": 5000,
+        "numero_pacage": "123456789",
     }
     moulinette_data = {"initial": data, "data": data}
 
