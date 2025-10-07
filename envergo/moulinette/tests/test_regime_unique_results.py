3cc4bc1614c74f5dccae6718670b4ce6ab4092f0
Thibault Jouannic <thibault@miximum.fr>
2025-10-07 10:28:03 +0200

    Make test case for "alignement" case

---
+++ envergo/moulinette/tests/test_regime_unique_results.py
@@ -0,0 +1,95 @@
+import pytest
+
+from envergo.geodata.conftest import france_map  # noqa
+from envergo.hedges.tests.factories import HedgeDataFactory, HedgeFactory
+from envergo.moulinette.models import MoulinetteHaie
+from envergo.moulinette.tests.factories import (
+    ConfigHaieFactory,
+    CriterionFactory,
+    RegulationFactory,
+)
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(autouse=True)
+def autouse_site(site):
+    pass
+
+
+@pytest.fixture(autouse=True)
+def alignementarbres_criteria(france_map):  # noqa
+    regulation = RegulationFactory(regulation="alignement_arbres")
+
+    criteria = [
+        CriterionFactory(
+            title="Alignement arbres > L350-3",
+            regulation=regulation,
+            evaluator="envergo.moulinette.regulations.alignementarbres.AlignementsArbres",
+            activation_map=france_map,
+            activation_mode="department_centroid",
+        ),
+    ]
+    return criteria
+
+
+def test_moulinette_result_alignement():
+    """Hedges with 100% "alignement d'arbres" are outside the unique procedure."""
+
+    ConfigHaieFactory(single_procedure=True)
+    hedges = HedgeDataFactory(
+        hedges=[
+            HedgeFactory(
+                additionalData={"type_haie": "alignement", "sur_parcelle_pac": False},
+            )
+        ]
+    )
+    data = {
+        "element": "haie",
+        "travaux": "destruction",
+        "profil": "autre",
+        "motif": "chemin_acces",
+        "reimplantation": "remplacement",
+        "localisation_pac": "non",
+        "haies": hedges,
+        "department": "44",
+    }
+    moulinette = MoulinetteHaie(data, data)
+    assert moulinette.is_evaluation_available()
+
+    # Update with this after the big moulinette refacto is merged
+    # moulinette_data = {"initial": data, "data": data}
+    # moulinette = MoulinetteHaie(moulinette_data)
+    # assert moulinette.is_valid(), moulinette.form_errors()
+
+    assert moulinette.result == "hors_regime_unique"
+
+
+def test_moulinette_result_non_alignement():
+    ConfigHaieFactory(single_procedure=True)
+    hedges = HedgeDataFactory(
+        hedges=[
+            HedgeFactory(
+                additionalData={"type_haie": "mixte", "sur_parcelle_pac": False},
+            )
+        ]
+    )
+    data = {
+        "element": "haie",
+        "travaux": "destruction",
+        "profil": "autre",
+        "motif": "chemin_acces",
+        "reimplantation": "remplacement",
+        "localisation_pac": "non",
+        "haies": hedges,
+        "department": "44",
+    }
+    moulinette = MoulinetteHaie(data, data)
+    assert moulinette.is_evaluation_available()
+
+    # Update with this after the big moulinette refacto is merged
+    # moulinette_data = {"initial": data, "data": data}
+    # moulinette = MoulinetteHaie(moulinette_data)
+    # assert moulinette.is_valid(), moulinette.form_errors()
+
+    assert moulinette.result == "declaration"
