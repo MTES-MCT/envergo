8dab12e1c9016c41cbcc8b36ddd82ecb77854199
Thibault Jouannic <thibault@miximum.fr>
2025-10-07 10:48:20 +0200

    Add interdit test case

--- envergo/moulinette/tests/test_regime_unique_results.py
+++ envergo/moulinette/tests/test_regime_unique_results.py
@@ -33,8 +33,23 @@ def alignementarbres_criteria(france_map):  # noqa
     return criteria
 
 
+@pytest.fixture(autouse=True)
+def conditionnalite_pac_criteria(france_map):  # noqa
+    regulation = RegulationFactory(regulation="conditionnalite_pac")
+    criteria = [
+        CriterionFactory(
+            title="Bonnes conditions agricoles et environnementales - Fiche VIII",
+            regulation=regulation,
+            evaluator="envergo.moulinette.regulations.conditionnalitepac.Bcae8",
+            activation_map=france_map,
+            activation_mode="department_centroid",
+        ),
+    ]
+    return criteria
+
+
 def test_moulinette_droit_constant():
-    """Hedges with 100% "alignement d'arbres" are outside the unique procedure."""
+    """When not in the single procedure case, returns the default result."""
 
     ConfigHaieFactory(single_procedure=False)
     hedges = HedgeDataFactory(
@@ -137,3 +152,43 @@ def test_moulinette_result_non_alignement():
     # assert moulinette.is_valid(), moulinette.form_errors()
 
     assert moulinette.result == "declaration"
+
+
+def test_moulinette_result_interdit():
+    config = ConfigHaieFactory(single_procedure=False)
+    hedges = HedgeDataFactory(
+        hedges=[
+            HedgeFactory(
+                additionalData={
+                    "type_haie": "mixte",
+                    "sur_parcelle_pac": True,
+                },
+            )
+        ]
+    )
+    data = {
+        "element": "haie",
+        "travaux": "destruction",
+        "profil": "autre",
+        "motif": "amelioration_culture",
+        "reimplantation": "non",
+        "localisation_pac": "oui",
+        "transfert_parcelles": "oui",
+        "meilleur_emplacement": "non",
+        "haies": hedges,
+        "department": "44",
+        "lineaire_total": 500,
+    }
+    moulinette = MoulinetteHaie(data, data)
+    assert moulinette.is_evaluation_available()
+
+    # Update with this after the big moulinette refacto is merged
+    # moulinette_data = {"initial": data, "data": data}
+    # moulinette = MoulinetteHaie(moulinette_data)
+    # assert moulinette.is_valid(), moulinette.form_errors()
+
+    assert moulinette.result == "interdit"
+
+    config.single_procedure = True
+    config.save()
+    assert moulinette.result == "interdit"
