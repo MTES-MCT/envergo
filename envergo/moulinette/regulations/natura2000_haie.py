dab0580fee9c3c458e72d93a3e215b80304ec5de
Thibault Jouannic <thibault@miximum.fr>
2025-10-13 15:14:35 +0200

    Update regulation evaluator names

--- envergo/moulinette/regulations/natura2000_haie.py
+++ envergo/moulinette/regulations/natura2000_haie.py
@@ -13,7 +13,7 @@ from envergo.moulinette.regulations import CriterionEvaluator, HaieRegulationEva
 
 
 class Natura2000HaieRegulation(HaieRegulationEvaluator):
-    choice_label = "Natura 2000 > Haie"
+    choice_label = "Natura 2000 Haie"
 
     LEVEL_MATRIX = {
         "soumis": "autorisation",
