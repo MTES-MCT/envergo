4990837432b10fc6d9642bbade2f326477effeda
EH <emgh@webeniste.net>
2025-10-30 11:36:39 +0100

    Get department

--- envergo/users/mixins.py
+++ envergo/users/mixins.py
@@ -7,14 +7,19 @@ from envergo.geodata.models import Department
 class InstructorDepartmentAuthorised(AccessMixin):
     """Authorize user according to project department"""
 
+    department = None
+
+    def get_departement(self, **kwargs):
+        """Get department from kwargs if available"""
+        return get_object_or_404(Department, department=self.kwargs["department"])
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return self.handle_no_permission()
 
         # Find department if exists and set object department attribute
-        self.department = get_object_or_404(
-            Department, department=self.kwargs["department"]
-        )
+        if not self.department:
+            self.department = self.get_departement(**kwargs)
 
         if (
             not request.user.is_superuser
