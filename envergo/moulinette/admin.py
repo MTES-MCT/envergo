be7ec5c8b7da88573488470157ce041f8b36602a
EH <emgh@webeniste.net>
2025-11-13 09:50:40 +0100

    Simplify display

--- envergo/moulinette/admin.py
+++ envergo/moulinette/admin.py
@@ -2,7 +2,6 @@ import json
 
 from django import forms
 from django.contrib import admin
-from django.contrib.admin.widgets import AutocompleteMixin
 from django.template.defaultfilters import truncatechars
 from django.template.loader import render_to_string
 from django.urls import reverse
@@ -89,64 +88,6 @@ class RegulationAdmin(admin.ModelAdmin):
         return obj.regulation
 
 
-class PerimeterAutocompleteWidget(AutocompleteMixin, forms.Select):
-    """AutocompleteWidget for perimeter"""
-
-    def __init__(self, *args, **kwargs):
-        self.admin_site = admin.site
-        super.__init__(*args, **kwargs)
-
-    def optgroups(self, name, value, attrs=None):
-        """Return a list of optgroups for this widget."""
-        groups = []
-        has_selected = False
-
-        for index, (option_value, option_label) in enumerate(self.choices):
-            if option_value is None:
-                option_value = ""
-
-            subgroup = []
-            if isinstance(option_label, (list, tuple)):
-                group_name = option_value
-                subindex = 0
-                choices = option_label
-            else:
-                group_name = None
-                subindex = None
-                choices = [(option_value, option_label)]
-            groups.append((group_name, subgroup, index))
-
-            for subvalue, sublabel in choices:
-                selected = (not has_selected or self.allow_multiple_selected) and str(
-                    subvalue
-                ) in value
-                has_selected |= selected
-                subgroup.append(
-                    self.create_option(
-                        name,
-                        subvalue,
-                        sublabel,
-                        selected,
-                        index,
-                        subindex=subindex,
-                        attrs=attrs,
-                    )
-                )
-                if subindex is not None:
-                    subindex += 1
-        return groups
-
-
-class PerimeterChoiceField(forms.ModelChoiceField):
-    """Field for perimeter with autocomplete and backend name title"""
-
-    to_field_name = "backend_name"
-    widget = PerimeterAutocompleteWidget
-
-    def label_from_instance(self, obj):
-        return obj.backend_name
-
-
 class CriterionAdminForm(forms.ModelForm):
     header = forms.CharField(
         label=_("Header"),
@@ -161,7 +102,6 @@ class CriterionAdminForm(forms.ModelForm):
             perimeter_field.label_from_instance = (
                 lambda perimeter: perimeter.backend_name
             )
-            perimeter_field.to_field_name = "backend_name"
 
         if "activation_map" in self.fields:
             self.fields["activation_map"].queryset = self.fields[
