c35a3420edfd343ec90c38f63e33a96330434b38
EH <emgh@webeniste.net>
2025-11-21 10:37:43 +0100

    Move to petition because error on utils

--- envergo/petitions/templatetags/petitions.py
+++ envergo/petitions/templatetags/petitions.py
@@ -1,3 +1,4 @@
+import re
 import uuid
 from datetime import date
 from typing import Literal
@@ -5,7 +6,9 @@ from typing import Literal
 from django import template
 from django.template import TemplateDoesNotExist
 from django.template.defaultfilters import date as date_filter
+from django.template.defaultfilters import stringfilter
 from django.template.loader import render_to_string
+from django.utils.html import urlize as _urlize
 from django.utils.safestring import mark_safe
 
 from envergo.hedges.models import TO_PLANT, TO_REMOVE
@@ -266,3 +269,16 @@ def display_ds_field(context, field_name):
 def has_edit_permission(user, project):
     """Check if the user can edit the project."""
     return project.has_user_as_department_instructor(user)
+
+
+@register.filter(is_safe=True, needs_autoescape=True)
+@stringfilter
+def urlize_html(value, blank=True, autoescape=True):
+    """Convert URLs in plain text into clickable links."""
+    # Remove existing tag a before urlize
+    clean = re.compile("</?a.*?>")
+    result = re.sub(clean, "", value)
+    result = _urlize(result, nofollow=True, autoescape=autoescape)
+    if blank:
+        result = result.replace("<a", '<a target="_blank"')
+    return mark_safe(result)
