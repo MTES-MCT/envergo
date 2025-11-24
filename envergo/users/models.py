f164cee7f351431c7ed1659b7389f82c17393a26
EH <emgh@webeniste.net>
2025-11-24 16:12:16 +0100

    Add is_instructor field on user model

--- envergo/users/models.py
+++ envergo/users/models.py
@@ -47,6 +47,12 @@ class User(AbstractUser):
     )
     access_haie = models.BooleanField(_("Access haie site"), default=False)
 
+    is_instructor = models.BooleanField(
+        _("Instructor"),
+        default=False,
+        help_text="""Instructeur pour tous les départements qui lui sont assignés.
+        Si cette case n'est pas cochée, la personne a le statut d'invitée.""",
+    )
     departments = models.ManyToManyField(
         "geodata.Department",
         verbose_name=_("Departements"),
