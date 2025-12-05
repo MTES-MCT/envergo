bd5b41341f987a26021be61c87d45c2099ea9dad
Thibault Jouannic <thibault@miximum.fr>
2025-12-05 10:47:06 +0100

    Display simulation list

--- envergo/petitions/models.py
+++ envergo/petitions/models.py
@@ -419,6 +419,7 @@ class Simulation(models.Model):
     is_active = models.BooleanField("Active ?", default=False)
     moulinette_url = models.URLField(_("Moulinette url"), max_length=2048)
     source = models.CharField("Auteur", choices=USER_TYPE, default=USER_TYPE.petitioner)
+    comment = models.TextField("Commentaire")
 
     created_at = models.DateTimeField(_("Date created"), default=timezone.now)
 
