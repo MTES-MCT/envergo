0daa08f920dcd8aad6faec8dccc1ce12ce7df1f4
Thibault Jouannic <thibault@miximum.fr>
2025-12-05 10:20:08 +0100

    Create default simulation upon project creation

--- envergo/petitions/views.py
+++ envergo/petitions/views.py
@@ -57,6 +57,7 @@ from envergo.petitions.models import (
     InvitationToken,
     LatestMessagerieAccess,
     PetitionProject,
+    Simulation,
     StatusLog,
 )
 from envergo.petitions.services import (
@@ -193,6 +194,12 @@ class PetitionProjectCreate(FormView):
                 petition_project.save()
 
                 StatusLog.objects.create(petition_project=petition_project)
+
+                Simulation.objects.create(
+                    project=petition_project,
+                    is_active=True,
+                    moulinette_url=petition_project.moulinette_url,
+                )
 
                 log_event(
                     "demande",
