ff9db38eecae4a71a3a0d6579c6c6ac1f40cd512
Thibault Jouannic <thibault@miximum.fr>
2025-12-19 17:52:59 +0100

    Active une invitation via un middleware (WIB)
    Message pour mon moi post-vacances. La stratégie est de se débarasser de
    la vue spécifique d'acceptation d'invitation, et de conserver le token
    stocké dans les cookies.
    
    Lorsqu'un utilisateur se connecte, le middleware extrait le token des
    cookies et vérifie l'existence d'une invitation.
    
    À faire :
    
     - vérifier le bon fonctionnement du middleware d'invitation
     - afficher des messages quand un token existe / une invitation est
       acceptée

--- envergo/petitions/middleware.py
+++ envergo/petitions/middleware.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from django.conf import settings
 
+from envergo.petitions.models import InvitationToken
+
 
 class StoreInvitationToken:
     """Store invitation tokens in session."""
@@ -11,7 +13,9 @@ class StoreInvitationToken:
 
     def __call__(self, request):
         new_token = request.GET.get("invitation_token")
+
         response = self.get_response(request)
+
         if new_token:
             lifetime = timedelta(30 * 13)  # 13 months
             response.set_cookie(
@@ -24,5 +28,26 @@ class StoreInvitationToken:
                 httponly=True,
                 samesite=settings.SESSION_COOKIE_SAMESITE,
             )
+
+        return response
+
+
+class ActivateInvitationMiddleware:
+    """Activate invitation tokens in session."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        response = self.get_response(request)
+
+        token = request.COOKIES.get(settings.INVITATION_TOKEN_COOKIE_NAME)
+        if token and request.user.is_authenticated:
+            invitation = InvitationToken.objects.filter(token=token).first()
+            if invitation and invitation.is_valid(request.user):
+                invitation.user = request.user
+                invitation.save()
+                response.delete_cookie(settings.INVITATION_TOKEN_COOKIE_NAME)
 
         return response
