d09858c8cbd3f1fd97e1aabbcb2be28c546bef74
Thibault Jouannic <thibault@miximum.fr>
2026-01-06 15:46:38 +0100

    Handle invitation tokens

--- envergo/petitions/middleware.py
+++ envergo/petitions/middleware.py
@@ -1,53 +1,88 @@
 from datetime import timedelta
 
 from django.conf import settings
+from django.contrib import messages
 
 from envergo.petitions.models import InvitationToken
 
 
-class StoreInvitationToken:
+class HandleInvitationTokenMiddleware:
     """Store invitation tokens in session."""
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        new_token = request.GET.get("invitation_token")
 
+        url_token = request.GET.get(settings.INVITATION_TOKEN_COOKIE_NAME)
+        cookie_token = request.COOKIES.get(settings.INVITATION_TOKEN_COOKIE_NAME)
+
+        # User is authenticated. We look for invitation tokens in session
+        # or in session data.
+        if request.user.is_authenticated:
+            if url_token:
+                self.process_token(request, url_token)
+
+            if cookie_token:
+                self.process_token(request, cookie_token)
+
+        # We process the invitation to prevent a 403 on the requested url
         response = self.get_response(request)
 
-        if new_token:
-            lifetime = timedelta(30 * 13)  # 13 months
-            response.set_cookie(
-                settings.INVITATION_TOKEN_COOKIE_NAME,
-                new_token,
-                max_age=lifetime.total_seconds(),
-                domain=settings.SESSION_COOKIE_DOMAIN,
-                path=settings.SESSION_COOKIE_PATH,
-                secure=settings.SESSION_COOKIE_SECURE or None,
-                httponly=True,
-                samesite=settings.SESSION_COOKIE_SAMESITE,
-            )
+        # Clear the cookie token if it exists
+        if request.user.is_authenticated and cookie_token:
+            self.clear_token(response)
+
+        # User is not authenticated and an invitation token is found in
+        # the url. We just store the token in session to be used later.
+        if url_token and not request.user.is_authenticated:
+            self.store_token(request, response, url_token)
 
         return response
 
+    def store_token(self, request, response, token):
+        """Store the given token in session."""
 
-class ActivateInvitationMiddleware:
-    """Activate invitation tokens in session."""
+        lifetime = timedelta(30 * 13)  # 13 months
+        response.set_cookie(
+            settings.INVITATION_TOKEN_COOKIE_NAME,
+            token,
+            max_age=lifetime.total_seconds(),
+            domain=settings.SESSION_COOKIE_DOMAIN,
+            path=settings.SESSION_COOKIE_PATH,
+            secure=settings.SESSION_COOKIE_SECURE or None,
+            httponly=True,
+            samesite=settings.SESSION_COOKIE_SAMESITE,
+        )
 
-    def __init__(self, get_response):
-        self.get_response = get_response
+        messages.info(
+            request,
+            """
+            Pour accéder au dossier en tant qu’invité,
+            connectez-vous ou créez un compte sur le Guichet Unique de la Haie.
+            """,
+        )
 
-    def __call__(self, request):
+    def process_token(self, request, token):
+        """Accepts the invitation."""
 
-        response = self.get_response(request)
+        invitation = InvitationToken.objects.filter(token=token).first()
 
-        token = request.COOKIES.get(settings.INVITATION_TOKEN_COOKIE_NAME)
-        if token and request.user.is_authenticated:
-            invitation = InvitationToken.objects.filter(token=token).first()
-            if invitation and invitation.is_valid(request.user):
+        if invitation:
+            if invitation.is_valid(request.user):
                 invitation.user = request.user
                 invitation.save()
-                response.delete_cookie(settings.INVITATION_TOKEN_COOKIE_NAME)
 
-        return response
+                messages.info(request, "Un dossier a été rattaché à votre compte.")
+            else:
+                messages.warning(
+                    request,
+                    """
+                    Le lien d'invitation utilisé n'est plus valide.
+                    Il a peut-être déjà été utilisé ou a expiré.
+                    Veuillez contacter la personne qui vous l'a transmis pour obtenir
+                    un nouveau lien.""",
+                )
+
+    def clear_token(self, response):
+        response.delete_cookie(settings.INVITATION_TOKEN_COOKIE_NAME)
