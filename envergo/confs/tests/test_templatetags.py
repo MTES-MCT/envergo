3157b4bdfb55888c0c7caa2ac73156c6556f7f03
EH <emgh@webeniste.net>
2026-01-15 10:20:49 +0100

    Add teaching top bar and ENV_NAME in context

---
+++ envergo/confs/tests/test_templatetags.py
@@ -0,0 +1,43 @@
+import pytest
+from django.template import Context, Template
+from django.test import RequestFactory, override_settings
+
+from envergo.confs.tests.factories import TopBarFactory
+
+pytestmark = pytest.mark.django_db
+
+
+def test_top_bar_displayed(site):
+    """Test top_bar data sent to template."""
+
+    # GIVEN an active top bar
+    topbar = TopBarFactory(site=site)
+    factory = RequestFactory()
+    request = factory.get("")
+    request.site = site
+    request.session = {}
+    # WHEN I want to show top bar in a template
+    template_html = "{% load confs %}{% top_bar %}"
+    context_data = {"request": request}
+    content = Template(template_html).render(Context(context_data))
+    # THEN this top bar text is in template
+    assert topbar.message_html in content
+    # AND teaching top bar is not in template
+    assert "Environnement de formation." not in content
+
+
+@override_settings(ENV_NAME="teaching")
+def test_teaching_top_bar_displayed(site):
+    """Test teaching top_bar is in template."""
+
+    # GIVEN a fake request
+    factory = RequestFactory()
+    request = factory.get("")
+    request.site = site
+    request.session = {}
+    # WHEN I want to show top bar in a template
+    template_html = "{% load confs %}{% top_bar %}"
+    context_data = {"request": request}
+    content = Template(template_html).render(Context(context_data))
+    # THEN teaching top bar is not in template
+    assert "Environnement de formation." in content
