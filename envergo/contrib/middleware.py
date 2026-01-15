a6b508c065b082b969e81155c31f74ffe1201617
Thibault Jouannic <thibault@miximum.fr>
2026-01-15 11:12:32 +0100

    Prevent error 500 when invalid domain
    Some invalid urls exist in the wild that generate exception when they
    are accessed.
    
    E.g https://envergo.osc-fr1.scalingo.io/avis/UEBXZC/
    
    Fixes https://sentry.incubateur.net/organizations/betagouv/issues/166469/?project=201

--- envergo/contrib/middleware.py
+++ envergo/contrib/middleware.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.http import HttpResponseRedirect
+
+logger = logging.getLogger(__name__)
 
 
 class SetUrlConfBasedOnSite:
@@ -15,7 +20,19 @@ class SetUrlConfBasedOnSite:
 
     def __call__(self, request):
         request.urlconf = "config.urls_amenagement"
-        site = Site.objects.get_current(request)
+        try:
+            site = Site.objects.get_current(request)
+        except Site.DoesNotExist:
+            # If the site is not found, it might be from an invalid url or from and old
+            # link lost somewhere in the wild.
+            # We try to redirect to the amenagement site to mitigate the problem as
+            # best as we can
+            logger.error(
+                f"Found url with bad domain in the wild: {request.get_host()}{request.get_full_path()}"
+            )
+            new_url = f"https://{settings.ENVERGO_AMENAGEMENT_DOMAIN}{request.get_full_path()}"
+            return HttpResponseRedirect(new_url)
+
         request.site = site
         request.base_template = "amenagement/base.html"
         if site.domain == settings.ENVERGO_HAIE_DOMAIN:
