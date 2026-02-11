ae861b5280219730caec7feb3b356919d31a0a98
EH <emgh@webeniste.net>
2026-02-11 10:26:29 +0100

    Add template

--- envergo/demos/urls.py
+++ envergo/demos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from envergo.demos.views import CatchmentArea, HedgeDensity
+from envergo.demos.views import CatchmentArea, HedgeDensity, HedgeDensityBuffer
 
 urlpatterns = [
     path("densite-haie/", HedgeDensity.as_view(), name="demo_density"),
-    path("densite-haie-2/", HedgeDensity.as_view(), name="demo_density_buffer"),
+    path("densite-haie-2/", HedgeDensityBuffer.as_view(), name="demo_density_buffer"),
     path("bassin-versant/", CatchmentArea.as_view(), name="demo_catchment_area"),
 ]
