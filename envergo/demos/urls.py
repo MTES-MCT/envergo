37d19b5be0540885ece57de3528288e3b68eaa94
EH <emgh@webeniste.net>
2026-02-17 15:33:38 +0100

    Rename url

--- envergo/demos/urls.py
+++ envergo/demos/urls.py
@@ -4,6 +4,10 @@ from envergo.demos.views import CatchmentArea, HedgeDensity, HedgeDensityBuffer
 
 urlpatterns = [
     path("densite-haie/", HedgeDensity.as_view(), name="demo_density"),
-    path("densite-haie-2/", HedgeDensityBuffer.as_view(), name="demo_density_buffer"),
+    path(
+        "densite-haie-projet/",
+        HedgeDensityBuffer.as_view(),
+        name="demo_density_project",
+    ),
     path("bassin-versant/", CatchmentArea.as_view(), name="demo_catchment_area"),
 ]
