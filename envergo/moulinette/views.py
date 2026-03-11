9b996f6d7d5906234a28feaf304e0c56a8920bff
EH <emgh@webeniste.net>
2026-03-11 12:22:37 +0100

    Improve code with PR review

--- envergo/moulinette/views.py
+++ envergo/moulinette/views.py
@@ -730,8 +730,6 @@ class ConfigHaieListView(InstructorDepartmentAuthorised, ListView):
         """Filter confighaie by user departments"""
 
         current_user = self.request.user
-        if not current_user.is_authenticated:
-            return self.queryset.none()
         if not current_user.is_superuser and not current_user.is_instructor:
             return self.queryset.none()
 
@@ -747,7 +745,7 @@ class ConfigHaieListView(InstructorDepartmentAuthorised, ListView):
 
     def get(self, request, *args, **kwargs):
         """Redirect to object detail if only one config is listed"""
-        self.object_list = self.get_queryset()
+        result = super().get(request, *args, **kwargs)
         if self.object_list.count() == 1:
             config = self.object_list.get()
             config_url = reverse(
@@ -758,7 +756,7 @@ class ConfigHaieListView(InstructorDepartmentAuthorised, ListView):
                 },
             )
             return HttpResponseRedirect(config_url)
-        return super().get(request, *args, **kwargs)
+        return result
 
 
 class ConfigHaieSettingsView(InstructorDepartmentAuthorised, DetailView):
