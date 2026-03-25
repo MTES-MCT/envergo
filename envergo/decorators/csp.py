636b22e8dea6eeff316041a06b86cc580011b00e
Thibault Jouannic <thibault@miximum.fr>
2026-03-25 15:26:49 +0100

    Fix tally csp error (~850 errors)
    Directive: script-src-elem + frame-src
    Blocked: https://tally.so/widgets/embed.js and https://tally.so iframe
    
    Since this is for a single page, override only the view csp instead of
    site wide

--- envergo/decorators/csp.py
+++ envergo/decorators/csp.py
@@ -40,3 +40,47 @@ def csp_override(config):
 def csp_report_only_override(config):
     """Override the Content-Security-Policy-Report-Only header for a view."""
     return _make_csp_decorator("_csp_ro_config", config)
+
+
+# WARNING This is not part of the django 6.0 backport
+def _make_csp_update_decorator(config_attr_name, config_attr_value):
+    """CSP update decorator factory.
+
+    This creates a decorator that updates existing csp headers with new values.
+    """
+
+    if not isinstance(config_attr_value, dict):
+        raise TypeError("CSP config should be a mapping.")
+
+    def decorator(view_func):
+        @wraps(view_func)
+        async def _wrapped_async_view(request, *args, **kwargs):
+            response = await view_func(request, *args, **kwargs)
+            config = getattr(response, config_attr_name, {})
+            config.update(config_attr_value)
+            setattr(response, config_attr_name, config)
+            return response
+
+        @wraps(view_func)
+        def _wrapped_sync_view(request, *args, **kwargs):
+            response = view_func(request, *args, **kwargs)
+            config = getattr(response, config_attr_name, {})
+            config.update(config_attr_value)
+            setattr(response, config_attr_name, config)
+            return response
+
+        if iscoroutinefunction(view_func):
+            return _wrapped_async_view
+        return _wrapped_sync_view
+
+    return decorator
+
+
+def csp_update(config):
+    """Update the Content-Security-Policy header for a view."""
+    return _make_csp_update_decorator("_csp_config", config)
+
+
+def csp_report_only_update(config):
+    """Update the Content-Security-Policy-Report-Only header for a view."""
+    return _make_csp_update_decorator("_csp_ro_config", config)
