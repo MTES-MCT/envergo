2f98b590467890e386d8f5416b5c94b2f59a8486
Thibault Jouannic <thibault@miximum.fr>
2026-04-09 16:45:12 +0200

    Quickfix: remove the "on land" sentinel
    The hedge density (around centroid method) has a sentinel check: if the
    project centroid is NOT in a "terres emergées" map, then when just
    return a static density value.
    
    Problem: the actual terres emergees maps are full of holes, because
    forests, for examples, are excluded.
    
    So half the time, the simulation is false because we wrongly omit to
    compute the density.
    
    This commit removes this check.

--- envergo/geodata/utils.py
+++ envergo/geodata/utils.py
@@ -649,19 +649,9 @@ def compute_hedge_density_around_point(point_geos, radius):
     circle = centroid_meter.buffer(radius)
     circle = circle.transform(EPSG_WGS84, clone=True)  # switch back to WGS84
 
-    # Check if the circle center is on land
-    on_land = (
-        Zone.objects.filter(map__map_type=MAP_TYPES.terres_emergees)
-        .filter(geometry__covers=point_geos)
-        .exists()
-    )
-    truncated_circle = None
-
-    if on_land:
-        # Remove the sea from the circle
-        truncated_circle = trim_land(circle)
-
-    if on_land and truncated_circle:
+    # Remove the sea from the circle
+    truncated_circle = trim_land(circle)
+    if truncated_circle:
         length, area_ha, density = compute_hedge_density_data(
             truncated_circle, epsg_utm
         )
