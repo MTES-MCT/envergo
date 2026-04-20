f4e0683e18e60e337b9f9dbd7bf597166f60b562
EH <emgh@webeniste.net>
2026-04-20 17:59:57 +0200

    Improve tests

--- envergo/moulinette/tests/test_commands.py
+++ envergo/moulinette/tests/test_commands.py
@@ -4,7 +4,10 @@ import pytest
 from django.core.management import call_command
 from django.test import override_settings
 
-from envergo.moulinette.tests.factories import MoulinetteTemplateFactory
+from envergo.moulinette.tests.factories import (
+    CriterionFactory,
+    MoulinetteTemplateFactory,
+)
 
 pytestmark = pytest.mark.django_db
 
@@ -12,15 +15,22 @@ pytestmark = pytest.mark.django_db
 @patch("envergo.utils.mattermost.notify")
 def test_dossier_submission_admin_alert(mock_notify):
     """Test obsolete moulinette template admin alert"""
+    criterion = CriterionFactory()
     # GIVEN a template with no existing template key
     MoulinetteTemplateFactory()
+    MoulinetteTemplateFactory(criterion=criterion, config=None)
     # WHEN command is called
     call_command("obsolete_moulinette_template_admin_alert")
-    # THEN notify should be called one
-    mock_notify.assert_called()
-    # AND message is related to config
-    args, kwargs = mock_notify.call_args_list[0]
+    # THEN notify should be called twice
+    assert mock_notify.call_count == 2
+    # AND first message is related to config
+    args, _ = mock_notify.call_args_list[0]
     assert 'La config Aménagement du département ["Loire-Atlantique (44)"]' in args[0]
+    assert " référence un gabarit moulinette obsolète" in args[0]
+    assert "amenagement" in args[1]
+    # AND second message is related to config
+    args, _ = mock_notify.call_args_list[1]
+    assert 'Le critère ["Zone humide"]' in args[0]
     assert " référence un gabarit moulinette obsolète" in args[0]
     assert "amenagement" in args[1]
 
