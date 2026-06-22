3e8da2a5d61f774b047335b3375c2082f4c24450
PY <py.dezaunay@gmail.com>
2026-06-22 08:49:04 +0200

    improve template

--- envergo/hedges/templatetags/hedges.py
+++ envergo/hedges/templatetags/hedges.py
@@ -3,6 +3,7 @@
 from django import template
 
 from envergo.hedges.models import LEVELS_OF_CONCERN
+from envergo.moulinette.regulations import HaieCriterionCategory
 
 register = template.Library()
 
@@ -13,3 +14,15 @@ LEVEL_DISPLAY_MAP = dict(LEVELS_OF_CONCERN)
 def level_of_concern_display(value):
     """Return the human-readable label for a level_of_concern value."""
     return LEVEL_DISPLAY_MAP.get(value, value or "")
+
+
+@register.filter
+def hedges_category(hedge_data, category):
+    to_remove = hedge_data.hedges().to_remove()
+    if category == HaieCriterionCategory.hru:
+        return to_remove.hru()
+    elif category == HaieCriterionCategory.ru:
+        return to_remove.ru()
+    else:
+        # category == HaieCriterionCategory.l350_3:
+        return to_remove.l350_3()
