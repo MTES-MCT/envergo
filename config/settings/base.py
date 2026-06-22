fcf25cb7b0bbe4f440578e7d20054c4674f86af2
Alexis A. <alexis.athlani@gmail.com>
2026-06-22 11:02:37 +0200

    refactor(petitions): move ask-new-link form id to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -388,6 +388,9 @@ TRANSFER_EVAL_EMAIL_FORM_ID = "mDzXgX"
 # Tally form used by a DDT(M) to fill in / update a department's contact info
 DEPARTMENT_CONTACT_FORM_ID = "Pd9b9e"
 
+# Tally form used by an invited instructor to ask for a new invitation link
+ASK_NEW_LINK_FORM_ID = "Gxol8e"
+
 ADMIN_OTP_REQUIRED = False
 
 GEOMETRICIAN_WEBINAR_FORM_URL = env(
