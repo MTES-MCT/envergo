610d94e93c533eef5df2c862df686e868e012e38
Thibault Jouannic <thibault@miximum.fr>
2026-08-20 11:28:15 +0200

    Upgrade the storage classes

--- envergo/utils/storages.py
+++ envergo/utils/storages.py
@@ -1,17 +1,42 @@
 from django.conf import settings
+from django.core.files.storage import storages
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = "static"
-    default_acl = "public-read"
+class PrivateMediaStorage(S3Boto3Storage):
+    """Default storage for sensitive files (evaluations, petitions, maps, etc.)."""
 
-
-class MediaRootS3Boto3Storage(S3Boto3Storage):
+    bucket_name = settings.AWS_PRIVATE_BUCKET_NAME
     location = "media"
+    default_acl = "private"
     file_overwrite = False
+    querystring_auth = True
+    querystring_expire = 3600
 
 
-class UploadS3Boto3Storage(S3Boto3Storage):
+class PrivateUploadStorage(S3Boto3Storage):
+    """Petitioner-submitted files (RequestFile)."""
+
+    bucket_name = settings.AWS_PRIVATE_BUCKET_NAME
     location = "upload"
-    bucket_name = settings.AWS_UPLOAD_BUCKET_NAME
+    default_acl = "private"
+    file_overwrite = False
+    querystring_auth = True
+    querystring_expire = 3600
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    """Public documents (HostedFile). Separate bucket, stable URLs."""
+
+    bucket_name = settings.AWS_PUBLIC_BUCKET_NAME
+    default_acl = "public-read"
+    file_overwrite = False
+    querystring_auth = False
+
+
+def get_public_storage():
+    """Return the public storage backend.
+
+    A plain function because Django migrations cannot serialize lambdas.
+    """
+    return storages["public"]
