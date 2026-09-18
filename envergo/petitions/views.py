03e4e9bd014d4febbaca5c25da46947329f35994
Thibault Jouannic <thibault@miximum.fr>
2026-09-18 14:10:34 +0200

    Sort the regulations in the view rather than tpl

--- envergo/petitions/views.py
+++ envergo/petitions/views.py
@@ -916,6 +916,9 @@ class PetitionProjectInstructorMixin(SingleObjectMixin):
         context["hedge_types"] = HedgeTypeFactory.build_from_context(
             single_procedure=self.object.config.single_procedure
         )
+        context["regulations"] = self.object.get_available_regulations().order_by(
+            "display_order"
+        )
 
         context.update(get_context_from_dn(self.object))
         context.update(self.object.moulinette_data)
